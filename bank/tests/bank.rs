c0426c8b28c1d573d4db181a4d76a1f62f5d8d7d
Thorsten Lorenz <thlorenz@gmx.de>
2024-02-21 19:09:58 -0700

    bank: tests logging modified accounts

--- bank/tests/bank.rs
+++ bank/tests/bank.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::collections::HashSet;
+
 use sleipnir_bank::bank::Bank;
 use solana_program_runtime::timings::ExecuteTimings;
 use solana_sdk::{clock::MAX_PROCESSING_AGE, genesis_config::create_genesis_config};
@@ -20,11 +22,22 @@ fn test_bank_one_system_instruction() {
     let (transaction_results, transaction_balances) = bank.load_execute_and_commit_transactions(
         &batch,
         MAX_PROCESSING_AGE,
-        false,
+        true,
         Default::default(),
         &mut timings,
         None,
     );
-    eprintln!("{:?}", transaction_results.execution_results);
+
+    eprintln!("{:?}", txs);
+    eprintln!("{:#?}", transaction_results.execution_results);
     eprintln!("{:#?}", transaction_balances);
+
+    for key in txs
+        .iter()
+        .flat_map(|tx| tx.message().account_keys().iter())
+        .collect::<HashSet<_>>()
+    {
+        let account = bank.get_account(key).unwrap();
+        eprintln!("{:?}: {:#?}", key, account);
+    }
 }
