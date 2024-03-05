efa956dfb14794a6d78277f9efe844199593a73b
Thorsten Lorenz <thlorenz@gmx.de>
2024-03-05 10:04:40 -0500

    mut: cannot use either rpc-client or rpc-client-api

--- sleipnir-rpc-client-api/src/lib.rs
+++ sleipnir-rpc-client-api/src/lib.rs
@@ -1,1 +1,2 @@
-pub use solana_rpc_client_api::*;
+// pub use solana_rpc_client_api::*;
+pub const MAYNEED_OUR_OWN_RPC_CLIENT_API: u8 = 1;
--- sleipnir-rpc-client/src/lib.rs
+++ sleipnir-rpc-client/src/lib.rs
@@ -1,1 +1,2 @@
-pub use solana_rpc_client::*;
+// pub use solana_rpc_client::*;
+pub const MAYNEED_OUR_OWN_RPC_CLIENT: u8 = 1;
