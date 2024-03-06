8839c5734c6158afe421dee0c514bbaeb0681ae4
Thorsten Lorenz <thlorenz@gmx.de>
2024-03-06 17:46:49 -0500

    mut: optionally using bank processor to easier triage our issue

--- sleipnir-mutator/tests/clone_non_executables.rs
+++ sleipnir-mutator/tests/clone_non_executables.rs
@@ -2,12 +2,25 @@ use sleipnir_bank::bank_dev_utils::init_logger;
 use sleipnir_mutator::Mutator;
 use sleipnir_program::sleipnir_authority_id;
 use solana_sdk::{genesis_config::ClusterType, hash::Hash};
+use test_tools::bank_transactions_processor::BankTransactionsProcessor;
+use test_tools::banking_stage_transactions_processor::BankingStageTransactionsProcessor;
+use test_tools::diagnostics::log_exec_details;
+use test_tools::traits::TransactionsProcessor;
 
 const SOLX_TIPS: &str = "SoLXtipsYqzgFguFCX6vw3JCtMChxmMacWdTpz2noRX";
+
+fn transactions_processor() -> Box<dyn TransactionsProcessor> {
+    if std::env::var("BANK_PROCESSOR").is_ok() {
+        Box::new(BankTransactionsProcessor::default())
+    } else {
+        Box::new(BankingStageTransactionsProcessor::default())
+    }
+}
 
 #[tokio::test]
 async fn clone_non_executable_without_data() {
     init_logger();
+
     let mutator = Mutator::default();
 
     let recent_blockhash = Hash::default();
@@ -20,4 +33,12 @@ async fn clone_non_executable_without_data() {
     assert_eq!(tx.signatures.len(), 1);
     assert_eq!(tx.signer_key(0, 0).unwrap(), &sleipnir_authority_id());
     assert_eq!(tx.message().account_keys.len(), 3);
+
+    let tx_processor = transactions_processor();
+    let result = tx_processor.process(vec![tx]).unwrap();
+
+    eprintln!("result: {:?}", result);
+
+    let (tx, exec_details) = result.transactions.values().next().unwrap();
+    log_exec_details(exec_details);
 }
