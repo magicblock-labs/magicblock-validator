a6009837a37e581578cbf69da04bdc118605927b
Thorsten Lorenz <thlorenz@gmx.de>
2024-03-07 09:58:46 -0500

    mut: working account clone including logs

--- sleipnir-mutator/tests/clone_non_executables.rs
+++ sleipnir-mutator/tests/clone_non_executables.rs
@@ -1,13 +1,17 @@
-use sleipnir_bank::bank_dev_utils::init_logger;
+use log::*;
+use test_tools::init_logger;
+
 use sleipnir_mutator::Mutator;
 use sleipnir_program::sleipnir_authority_id;
 use solana_sdk::{genesis_config::ClusterType, hash::Hash};
+use test_tools::account::{fund_account_addr, get_account_addr};
 use test_tools::bank_transactions_processor::BankTransactionsProcessor;
 use test_tools::banking_stage_transactions_processor::BankingStageTransactionsProcessor;
 use test_tools::diagnostics::log_exec_details;
 use test_tools::traits::TransactionsProcessor;
 
 const SOLX_TIPS: &str = "SoLXtipsYqzgFguFCX6vw3JCtMChxmMacWdTpz2noRX";
+const LUZIFER: &str = "LuzifKo4E6QCF5r4uQmqbyko7zLS5WgayynivnCbtzk";
 
 fn transactions_processor() -> Box<dyn TransactionsProcessor> {
     if std::env::var("BANK_PROCESSOR").is_ok() {
@@ -19,8 +23,7 @@ fn transactions_processor() -> Box<dyn TransactionsProcessor> {
 
 #[tokio::test]
 async fn clone_non_executable_without_data() {
-    init_logger();
-
+    init_logger!();
     let mutator = Mutator::default();
 
     let recent_blockhash = Hash::default();
@@ -35,10 +38,16 @@ async fn clone_non_executable_without_data() {
     assert_eq!(tx.message().account_keys.len(), 3);
 
     let tx_processor = transactions_processor();
+
+    // TODO: we need to fund Luzifer at startup instead of doing it here
+    fund_account_addr(tx_processor.bank(), LUZIFER, u64::MAX / 2);
+
     let result = tx_processor.process(vec![tx]).unwrap();
 
-    eprintln!("result: {:?}", result);
-
-    let (tx, exec_details) = result.transactions.values().next().unwrap();
+    let (_, exec_details) = result.transactions.values().next().unwrap();
     log_exec_details(exec_details);
+    debug!(
+        "SolxTips account: {:#?}",
+        get_account_addr(tx_processor.bank(), SOLX_TIPS)
+    );
 }
