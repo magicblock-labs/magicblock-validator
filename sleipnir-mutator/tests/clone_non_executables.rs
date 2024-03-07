07039f3e387f3638de938918fbd532425f8cc5c4
Thorsten Lorenz <thlorenz@gmx.de>
2024-03-07 10:27:05 -0500

    mut: cloning account with data

--- sleipnir-mutator/tests/clone_non_executables.rs
+++ sleipnir-mutator/tests/clone_non_executables.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
+
 use log::*;
 use solana_sdk::account::Account;
 use solana_sdk::native_token::LAMPORTS_PER_SOL;
+use solana_sdk::pubkey::Pubkey;
 use solana_sdk::system_program;
 use solana_sdk::transaction::Transaction;
 use test_tools::{init_logger, transactions_processor};
@@ -13,7 +16,9 @@ use test_tools::account::{fund_account_addr, get_account_addr};
 use test_tools::diagnostics::log_exec_details;
 use test_tools::traits::TransactionsProcessor;
 
+const SOLX_PROG: &str = "SoLXmnP9JvL6vJ7TN1VqtTxqsc2izmPfF9CsMDEuRzJ";
 const SOLX_TIPS: &str = "SoLXtipsYqzgFguFCX6vw3JCtMChxmMacWdTpz2noRX";
+const SOLX_POST: &str = "5eYk1TwtEwsUTqF9FHhm6tdmvu45csFkKbC4W217TAts";
 const LUZIFER: &str = "LuzifKo4E6QCF5r4uQmqbyko7zLS5WgayynivnCbtzk";
 
 fn fund_luzifer(bank: &Box<dyn TransactionsProcessor>) {
@@ -68,6 +73,42 @@ async fn clone_non_executable_without_data() {
             assert!(l > LAMPORTS_PER_SOL);
             assert!(d.is_empty());
             assert_eq!(o, system_program::id());
+            assert_eq!(r, u64::MAX);
+        }
+    );
+}
+
+#[tokio::test]
+async fn clone_non_executable_with_data() {
+    init_logger!();
+
+    let tx_processor = transactions_processor();
+    fund_luzifer(&tx_processor);
+
+    let tx = verified_tx_to_clone_from_devnet(SOLX_POST).await;
+    let result = tx_processor.process(vec![tx]).unwrap();
+
+    let (_, exec_details) = result.transactions.values().next().unwrap();
+    log_exec_details(exec_details);
+    let solx_post: Account = get_account_addr(tx_processor.bank(), SOLX_POST)
+        .unwrap()
+        .into();
+
+    trace!("SolxPost account: {:#?}", solx_post);
+
+    let solx_prog = Pubkey::from_str(SOLX_PROG).unwrap();
+    assert_matches!(
+        solx_post,
+        Account {
+            lamports: l,
+            data: d,
+            owner: o,
+            executable: false,
+            rent_epoch: r
+        } => {
+            assert!(l > 0);
+            assert_eq!(d.len(), 1180);
+            assert_eq!(o, solx_prog);
             assert_eq!(r, u64::MAX);
         }
     );
