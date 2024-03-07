a5c5fe0d747109cfc25dff7827832ff5ef707932
Thorsten Lorenz <thlorenz@gmx.de>
2024-03-07 10:20:07 -0500

    mut: test separated into reusable parts

--- sleipnir-mutator/tests/clone_non_executables.rs
+++ sleipnir-mutator/tests/clone_non_executables.rs
@@ -1,34 +1,32 @@
 use log::*;
-use test_tools::init_logger;
+use solana_sdk::account::Account;
+use solana_sdk::native_token::LAMPORTS_PER_SOL;
+use solana_sdk::system_program;
+use solana_sdk::transaction::Transaction;
+use test_tools::{init_logger, transactions_processor};
 
+use assert_matches::assert_matches;
 use sleipnir_mutator::Mutator;
 use sleipnir_program::sleipnir_authority_id;
 use solana_sdk::{genesis_config::ClusterType, hash::Hash};
 use test_tools::account::{fund_account_addr, get_account_addr};
-use test_tools::bank_transactions_processor::BankTransactionsProcessor;
-use test_tools::banking_stage_transactions_processor::BankingStageTransactionsProcessor;
 use test_tools::diagnostics::log_exec_details;
 use test_tools::traits::TransactionsProcessor;
 
 const SOLX_TIPS: &str = "SoLXtipsYqzgFguFCX6vw3JCtMChxmMacWdTpz2noRX";
 const LUZIFER: &str = "LuzifKo4E6QCF5r4uQmqbyko7zLS5WgayynivnCbtzk";
 
-fn transactions_processor() -> Box<dyn TransactionsProcessor> {
-    if std::env::var("BANK_PROCESSOR").is_ok() {
-        Box::new(BankTransactionsProcessor::default())
-    } else {
-        Box::new(BankingStageTransactionsProcessor::default())
-    }
+fn fund_luzifer(bank: &Box<dyn TransactionsProcessor>) {
+    // TODO: we need to fund Luzifer at startup instead of doing it here
+    fund_account_addr(bank.bank(), LUZIFER, u64::MAX / 2);
 }
 
-#[tokio::test]
-async fn clone_non_executable_without_data() {
-    init_logger!();
+async fn verified_tx_to_clone_from_devnet(addr: &str) -> Transaction {
     let mutator = Mutator::default();
 
     let recent_blockhash = Hash::default();
     let tx = mutator
-        .transaction_to_clone_account_from_cluster(ClusterType::Devnet, SOLX_TIPS, recent_blockhash)
+        .transaction_to_clone_account_from_cluster(ClusterType::Devnet, addr, recent_blockhash)
         .await
         .expect("Failed to create clone transaction");
 
@@ -37,15 +35,40 @@ async fn clone_non_executable_without_data() {
     assert_eq!(tx.signer_key(0, 0).unwrap(), &sleipnir_authority_id());
     assert_eq!(tx.message().account_keys.len(), 3);
 
+    tx
+}
+
+#[tokio::test]
+async fn clone_non_executable_without_data() {
+    init_logger!();
+
     let tx_processor = transactions_processor();
+    fund_luzifer(&tx_processor);
 
-    // TODO: we need to fund Luzifer at startup instead of doing it here
-    fund_account_addr(tx_processor.bank(), LUZIFER, u64::MAX / 2);
-
+    let tx = verified_tx_to_clone_from_devnet(SOLX_TIPS).await;
     let result = tx_processor.process(vec![tx]).unwrap();
 
     let (_, exec_details) = result.transactions.values().next().unwrap();
     log_exec_details(exec_details);
-    let solx_tips = get_account_addr(tx_processor.bank(), SOLX_TIPS).unwrap();
+    let solx_tips: Account = get_account_addr(tx_processor.bank(), SOLX_TIPS)
+        .unwrap()
+        .into();
+
     trace!("SolxTips account: {:#?}", solx_tips);
+
+    assert_matches!(
+        solx_tips,
+        Account {
+            lamports: l,
+            data: d,
+            owner: o,
+            executable: false,
+            rent_epoch: r
+        } => {
+            assert!(l > LAMPORTS_PER_SOL);
+            assert!(d.is_empty());
+            assert_eq!(o, system_program::id());
+            assert_eq!(r, u64::MAX);
+        }
+    );
 }
--- test-tools/src/lib.rs
+++ test-tools/src/lib.rs
@@ -1,5 +1,17 @@
+use bank_transactions_processor::BankTransactionsProcessor;
+use banking_stage_transactions_processor::BankingStageTransactionsProcessor;
+use traits::TransactionsProcessor;
+
 pub mod account;
 pub mod bank_transactions_processor;
 pub mod banking_stage_transactions_processor;
 pub mod diagnostics;
 pub mod traits;
+
+pub fn transactions_processor() -> Box<dyn TransactionsProcessor> {
+    if std::env::var("PROCESSOR_BANK").is_ok() {
+        Box::new(BankTransactionsProcessor::default())
+    } else {
+        Box::new(BankingStageTransactionsProcessor::default())
+    }
+}
