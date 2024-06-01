6fe0269964f4c3cc6cc2d5b9accb7c396f36db4f
Thorsten Lorenz <thlorenz@gmx.de>
2024-06-01 12:09:07 +0100

    feat: account locks

--- sleipnir-accounts-db/src/accounts.rs
+++ sleipnir-accounts-db/src/accounts.rs
@@ -1,26 +1,38 @@
-use crate::accounts_db::AccountsDb;
 use crate::accounts_index::ZeroLamport;
 use crate::storable_accounts::StorableAccounts;
+use crate::{account_locks::AccountLocks, accounts_db::AccountsDb};
+use log::debug;
 use solana_frozen_abi_macro::AbiExample;
+use solana_sdk::transaction::{
+    Result, SanitizedTransaction, TransactionAccountLocks, TransactionError,
+};
 use solana_sdk::{
     account::{AccountSharedData, ReadableAccount},
     clock::Slot,
     pubkey::Pubkey,
 };
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 #[derive(Debug, AbiExample)]
 pub struct Accounts {
     /// Single global AccountsDb
     pub accounts_db: Arc<AccountsDb>,
-    // NOTE: we may need the account locks here
+    /// Set of read-only and writable accounts which are currently
+    /// being processed by banking threads
+    pub(crate) account_locks: Mutex<AccountLocks>,
 }
 
 impl Accounts {
     pub fn new(accounts_db: Arc<AccountsDb>) -> Self {
-        Self { accounts_db }
+        Self {
+            accounts_db,
+            account_locks: Mutex::<AccountLocks>::default(),
+        }
     }
 
+    // -----------------
+    // Load/Store Accounts
+    // -----------------
     pub fn store_accounts_cached<
         'a,
         T: ReadableAccount + Sync + ZeroLamport + 'a,
@@ -36,5 +48,130 @@ impl Accounts {
         pubkey: &Pubkey,
     ) -> Option<(AccountSharedData, Slot)> {
         self.accounts_db.load_with_slot(pubkey)
+    }
+
+    // -----------------
+    // Account Locks
+    // -----------------
+    /// This function will prevent multiple threads from modifying the same account state at the
+    /// same time
+    #[must_use]
+    #[allow(clippy::needless_collect)]
+    pub fn lock_accounts<'a>(
+        &self,
+        txs: impl Iterator<Item = &'a SanitizedTransaction>,
+        tx_account_lock_limit: usize,
+    ) -> Vec<Result<()>> {
+        let tx_account_locks_results: Vec<Result<_>> = txs
+            .map(|tx| tx.get_account_locks(tx_account_lock_limit))
+            .collect();
+        self.lock_accounts_inner(tx_account_locks_results)
+    }
+
+    #[must_use]
+    #[allow(clippy::needless_collect)]
+    pub fn lock_accounts_with_results<'a>(
+        &self,
+        txs: impl Iterator<Item = &'a SanitizedTransaction>,
+        results: impl Iterator<Item = Result<()>>,
+        tx_account_lock_limit: usize,
+    ) -> Vec<Result<()>> {
+        let tx_account_locks_results: Vec<Result<_>> = txs
+            .zip(results)
+            .map(|(tx, result)| match result {
+                Ok(()) => tx.get_account_locks(tx_account_lock_limit),
+                Err(err) => Err(err),
+            })
+            .collect();
+        self.lock_accounts_inner(tx_account_locks_results)
+    }
+
+    #[must_use]
+    fn lock_accounts_inner(
+        &self,
+        tx_account_locks_results: Vec<Result<TransactionAccountLocks>>,
+    ) -> Vec<Result<()>> {
+        let account_locks = &mut self.account_locks.lock().unwrap();
+        tx_account_locks_results
+            .into_iter()
+            .map(|tx_account_locks_result| match tx_account_locks_result {
+                Ok(tx_account_locks) => self.lock_account(
+                    account_locks,
+                    tx_account_locks.writable,
+                    tx_account_locks.readonly,
+                ),
+                Err(err) => Err(err),
+            })
+            .collect()
+    }
+
+    fn lock_account(
+        &self,
+        account_locks: &mut AccountLocks,
+        writable_keys: Vec<&Pubkey>,
+        readonly_keys: Vec<&Pubkey>,
+    ) -> Result<()> {
+        for k in writable_keys.iter() {
+            if account_locks.is_locked_write(k)
+                || account_locks.is_locked_readonly(k)
+            {
+                debug!("Writable account in use: {:?}", k);
+                return Err(TransactionError::AccountInUse);
+            }
+        }
+        for k in readonly_keys.iter() {
+            if account_locks.is_locked_write(k) {
+                debug!("Read-only account in use: {:?}", k);
+                return Err(TransactionError::AccountInUse);
+            }
+        }
+
+        for k in writable_keys {
+            account_locks.write_locks.insert(*k);
+        }
+
+        for k in readonly_keys {
+            if !account_locks.lock_readonly(k) {
+                account_locks.insert_new_readonly(k);
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Once accounts are unlocked, new transactions that modify that state can enter the pipeline
+    #[allow(clippy::needless_collect)]
+    pub fn unlock_accounts<'a>(
+        &self,
+        txs: impl Iterator<Item = &'a SanitizedTransaction>,
+        results: &[Result<()>],
+    ) {
+        let keys: Vec<_> = txs
+            .zip(results)
+            .filter(|(_, res)| res.is_ok())
+            .map(|(tx, _)| tx.get_account_locks_unchecked())
+            .collect();
+        let mut account_locks = self.account_locks.lock().unwrap();
+        keys.into_iter().for_each(|keys| {
+            self.unlock_account(
+                &mut account_locks,
+                keys.writable,
+                keys.readonly,
+            );
+        });
+    }
+
+    fn unlock_account(
+        &self,
+        account_locks: &mut AccountLocks,
+        writable_keys: Vec<&Pubkey>,
+        readonly_keys: Vec<&Pubkey>,
+    ) {
+        for k in writable_keys {
+            account_locks.unlock_write(k);
+        }
+        for k in readonly_keys {
+            account_locks.unlock_readonly(k);
+        }
     }
 }
--- sleipnir-accounts-db/src/lib.rs
+++ sleipnir-accounts-db/src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(unused)]
 
 pub mod account_info;
+mod account_locks;
 pub mod accounts;
 pub mod accounts_cache;
 pub mod accounts_db;
