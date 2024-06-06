c07ff44ae77293f4c979ad9614a4f746eedd92f1
Thorsten Lorenz <thlorenz@gmx.de>
2024-06-06 11:03:06 +0100

    chore: circhash treats duplicate account entries correctly

--- utils/circular-hashmap/src/lib.rs
+++ utils/circular-hashmap/src/lib.rs
@@ -6,6 +6,12 @@ use std::{
     },
 };
 
+#[derive(Debug, Clone)]
+pub struct CountedEntry<V: Clone> {
+    value: V,
+    count: usize,
+}
+
 // -----------------
 // SharedMap
 // -----------------
@@ -13,7 +19,7 @@ use std::{
 /// exposing query methods.
 /// Consider it a limited interface for the [CircularHashMap].
 #[derive(Debug)]
-pub struct SharedMap<K, V>(Arc<RwLock<HashMap<K, V>>>)
+pub struct SharedMap<K, V>(Arc<RwLock<HashMap<K, CountedEntry<V>>>>)
 where
     K: PartialEq + Eq + std::hash::Hash + Clone,
     V: Clone;
@@ -24,7 +30,11 @@ where
     V: Clone,
 {
     pub fn get(&self, key: &K) -> Option<V> {
-        self.0.read().expect("RwLock poisoned").get(key).cloned()
+        self.0
+            .read()
+            .expect("RwLock poisoned")
+            .get(key)
+            .map(|e| e.value.clone())
     }
 
     pub fn len(&self) -> usize {
@@ -47,7 +57,7 @@ where
     K: PartialEq + Eq + std::hash::Hash + Clone,
     V: Clone,
 {
-    map: Arc<RwLock<HashMap<K, V>>>,
+    map: Arc<RwLock<HashMap<K, CountedEntry<V>>>>,
     vec: Arc<RwLock<Vec<K>>>,
     next_vec_index: AtomicUsize,
     max_size: usize,
@@ -61,7 +71,7 @@ where
     /// Creates a new CircularHashMap with the given max size.
     pub fn new(max_size: usize) -> Self {
         CircularHashMap {
-            map: Arc::<RwLock<HashMap<K, V>>>::default(),
+            map: Arc::<RwLock<HashMap<K, CountedEntry<V>>>>::default(),
             vec: Arc::new(RwLock::new(Vec::with_capacity(max_size))),
             next_vec_index: AtomicUsize::default(),
             max_size,
@@ -71,6 +81,26 @@ where
     /// Insert a new key-value pair into the map.
     /// If the map is full the oldest element is removed.
     pub fn insert(&self, key: K, value: V) {
+        // While inserting a new entry we ensure that our cache size doesn't grow larger
+        // than the max size.
+        // Thus we evict entries inserted before by circling in our ring buffer.
+        // However if the same key is inserted multiple times we need to only remove it from
+        // the map once we evict the latest entry of that key from our ring buffer.
+        // To that end we count how many times the entry was inserted and decrease that each time
+        // we find it in our ring buffer. Only once that count goes to zero do we evict it from the
+        // map.
+
+        // 1. Insert the new entry
+        let entry = if let Some(mut entry) = self.map_remove(&key) {
+            entry.count += 1;
+            entry.value = value;
+            entry
+        } else {
+            CountedEntry { value, count: 1 }
+        };
+        self.map_insert(key.clone(), entry);
+
+        // 2. Move the index in our ring buffer
         let vec_index = self
             .next_vec_index
             .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |x| {
@@ -78,14 +108,13 @@ where
             })
             .unwrap();
 
-        // If the map is full we remove the oldest element
+        // 3. Use the ring buffer to tell us if we need to remove an existing entry
         if self.vec_len() >= self.max_size {
             let old_key = self.vec_replace(vec_index, key.clone());
-            self.map_remove(&old_key);
+            self.map_decrease_count_and_maybe_remove(&old_key);
         } else {
             self.vec_push(key.clone());
         }
-        self.map_insert(key, value);
     }
 
     pub fn shared_map(&self) -> SharedMap<K, V> {
@@ -107,8 +136,26 @@ where
         )
     }
 
-    fn map_remove(&self, key: &K) {
-        self.map.write().expect("RwLock map poisoned").remove(key);
+    fn map_remove(&self, key: &K) -> Option<CountedEntry<V>> {
+        self.map.write().expect("RwLock map poisoned").remove(key)
+    }
+
+    fn map_decrease_count_and_maybe_remove(&self, key: &K) {
+        // If a particular entry was updated multiple times it is present in our ring buffer
+        // at multiple indexes. We want to remove it only once we find the last of those.
+        let remove = if let Some(entry) =
+            self.map.write().expect("RwLock map poisoned").get_mut(key)
+        {
+            entry.count -= 1;
+            entry.count == 0
+        } else {
+            false
+        };
+
+        // This happens rarely for accounts that don't see updates for a long time
+        if remove {
+            self.map_remove(key);
+        }
     }
 
     fn map_contains_key(&self, key: &K) -> bool {
@@ -118,7 +165,7 @@ where
             .contains_key(key)
     }
 
-    fn map_insert(&self, key: K, value: V) {
+    fn map_insert(&self, key: K, value: CountedEntry<V>) {
         self.map
             .write()
             .expect("RwLock map poisoned")
@@ -140,7 +187,7 @@ where
             .read()
             .expect("RwLock map poisoned")
             .get(key)
-            .cloned()
+            .map(|entry| entry.value.clone())
     }
 
     /// Get the number of elements stored in the map.
@@ -164,7 +211,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_circular_hashmap() {
+    fn test_circular_hashmap_inserting_different_keys() {
         let map = CircularHashMap::new(3);
 
         map.insert(1, 1);
@@ -208,5 +255,47 @@ mod tests {
         assert_eq!(map.get_cloned(&5), Some(5));
         assert_eq!(map.get_cloned(&6), Some(6));
         assert_eq!(map.get_cloned(&7), Some(7));
+    }
+
+    #[test]
+    fn test_circular_hashmap_inserting_same_key() {
+        let map = CircularHashMap::new(3);
+
+        map.insert(1, 1);
+        assert_eq!(map.get_cloned(&1), Some(1));
+        assert_eq!(map.len(), 1);
+
+        map.insert(1, 2);
+        assert_eq!(map.get_cloned(&1), Some(2));
+        assert_eq!(map.len(), 1);
+
+        map.insert(1, 3);
+        assert_eq!(map.get_cloned(&1), Some(3));
+        assert_eq!(map.len(), 1);
+
+        map.insert(2, 2);
+        assert_eq!(map.len(), 2);
+        assert_eq!(map.get_cloned(&1), Some(3));
+        assert_eq!(map.get_cloned(&2), Some(2));
+
+        map.insert(3, 3);
+        assert_eq!(map.len(), 3);
+        assert_eq!(map.get_cloned(&1), Some(3));
+        assert_eq!(map.get_cloned(&2), Some(2));
+        assert_eq!(map.get_cloned(&3), Some(3));
+
+        map.insert(1, 4);
+        assert_eq!(map.get_cloned(&1), Some(4));
+        assert_eq!(map.len(), 3);
+
+        // The fourth key causes another one to be removed
+        map.insert(4, 4);
+        assert_eq!(map.len(), 3);
+        // 1 was updated last so it should still be there
+        assert_eq!(map.get_cloned(&1), Some(4));
+        // 2 was updated before 3 was inserted and before 1 was updated
+        assert!(!map.contains_key(&2));
+        assert_eq!(map.get_cloned(&3), Some(3));
+        assert_eq!(map.get_cloned(&4), Some(4));
     }
 }
