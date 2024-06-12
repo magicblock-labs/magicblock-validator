7426e4da2641bba12a733932a1daac15f5e482ad
Thorsten Lorenz <thlorenz@gmx.de>
2024-06-12 12:41:52 +0100

    chore: mutator exposes account modification types

--- sleipnir-mutator/src/lib.rs
+++ sleipnir-mutator/src/lib.rs
@@ -6,5 +6,5 @@ pub mod errors;
 pub mod mutator;
 mod program_account;
 
-pub use account_modification::AccountModification;
+pub use account_modification::{AccountModification, ModifyAccountOpts};
 pub use cluster::*;
