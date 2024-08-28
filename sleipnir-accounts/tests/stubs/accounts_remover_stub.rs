8c84e002f8e4e6c9ae196a591f0a2d3ba47e6775
Thorsten Lorenz <thlorenz@gmx.de>
2024-08-28 17:17:15 +0400

    fix: test stub trait impl

--- sleipnir-accounts/tests/stubs/accounts_remover_stub.rs
+++ sleipnir-accounts/tests/stubs/accounts_remover_stub.rs
@@ -15,7 +15,7 @@ impl AccountsRemover for AccountsRemoverStub {
         unimplemented!("AccountsRemoverStub::request_accounts_removal not expected to be called during tests")
     }
 
-    fn has_accounts_pending_removal(&self) -> bool {
-        unimplemented!("AccountsRemoverStub::has_accounts_pending_removal not expected to be called during tests")
+    fn accounts_pending_removal(&self) -> HashSet<Pubkey> {
+        unimplemented!("AccountsRemoverStub::accounts_pending_removal not expected to be called during tests")
     }
 }
