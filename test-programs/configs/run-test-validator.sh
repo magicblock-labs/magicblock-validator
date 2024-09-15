89b015d8dbe225939f2d7f0ee80c9a789bc8bada
Thorsten Lorenz <thlorenz@gmx.de>
2024-09-15 10:08:35 +0800

    chore: move configs one level up

--- test-programs/configs/run-test-validator.sh
+++ test-programs/configs/run-test-validator.sh
@@ -7,10 +7,10 @@ solana-test-validator \
   10000 \
   --bpf-program \
   DELeGGvXpWV2fqJUhqcF5ZSYMS4JTLjteaAMARRSaeSh \
-  $DIR/../elfs/dlp.so \
+  $DIR/../schedulecommit/elfs/dlp.so \
   --bpf-program \
   9hgprgZiRWmy8KkfvUuaVkDGrqo9GzeXMohwq6BazgUY \
-  $DIR/../elfs/schedulecommit_program.so \
+  $DIR/../schedulecommit/elfs/schedulecommit_program.so \
   --bpf-program \
   4RaQH3CUBMSMQsSHPVaww2ifeNEEuaDZjF9CUdFwr3xr \
-  $DIR/../elfs/schedulecommit_test_security.so
+  $DIR/../schedulecommit/elfs/schedulecommit_test_security.so
