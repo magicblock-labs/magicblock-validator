c4d76ade02ef3c62be517690af08027df5067254
Vincent Brunet <vincent.brunet.us@gmail.com>
2024-09-17 12:19:32 +0800

    progress-save

--- sleipnir-account-updates/src/remote_account_updates_worker.rs
+++ sleipnir-account-updates/src/remote_account_updates_worker.rs
@@ -1,24 +1,21 @@
 use std::{
-    cmp::max,
-    collections::{hash_map::Entry, HashMap},
+    collections::HashMap,
     sync::{Arc, RwLock},
+    time::Duration,
 };
 
 use conjunto_transwise::RpcProviderConfig;
-use futures_util::StreamExt;
 use log::*;
-use solana_account_decoder::UiDataSliceConfig;
-use solana_pubsub_client::nonblocking::pubsub_client::PubsubClient;
-use solana_rpc_client_api::config::RpcAccountInfoConfig;
-use solana_sdk::{
-    clock::Slot, commitment_config::CommitmentConfig, pubkey::Pubkey,
-};
+use solana_sdk::{clock::Slot, pubkey::Pubkey};
 use thiserror::Error;
-use tokio::sync::mpsc::{
-    unbounded_channel, UnboundedReceiver, UnboundedSender,
+use tokio::{
+    sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+    task::JoinHandle,
+    time::sleep,
 };
-use tokio_stream::StreamMap;
 use tokio_util::sync::CancellationToken;
+
+use crate::RemoteAccountUpdatesShard;
 
 #[derive(Debug, Error)]
 pub enum RemoteAccountUpdatesWorkerError {
@@ -29,10 +26,18 @@ pub enum RemoteAccountUpdatesWorkerError {
     ),
 }
 
+struct RemoteAccountUpdatesWorkerRunner {
+    shard: RemoteAccountUpdatesShard,
+    monitoring_request_sender: UnboundedSender<Pubkey>,
+    cancellation_token: CancellationToken,
+    join_handle: JoinHandle<()>,
+}
+
 pub struct RemoteAccountUpdatesWorker {
     rpc_provider_config: RpcProviderConfig,
     monitoring_request_receiver: UnboundedReceiver<Pubkey>,
     monitoring_request_sender: UnboundedSender<Pubkey>,
+    runners: RwLock<Vec<RemoteAccountUpdatesWorkerRunner>>,
     last_known_update_slots: Arc<RwLock<HashMap<Pubkey, Slot>>>,
 }
 
@@ -44,6 +49,7 @@ impl RemoteAccountUpdatesWorker {
             rpc_provider_config,
             monitoring_request_receiver,
             monitoring_request_sender,
+            runners: Default::default(),
             last_known_update_slots: Default::default(),
         }
     }
@@ -62,62 +68,11 @@ impl RemoteAccountUpdatesWorker {
         &mut self,
         cancellation_token: CancellationToken,
     ) -> Result<(), RemoteAccountUpdatesWorkerError> {
-        // Create a pubsub client
-        let pubsub_client =
-            PubsubClient::new(self.rpc_provider_config.ws_url())
-                .await
-                .map_err(RemoteAccountUpdatesWorkerError::PubsubClientError)?;
-        // For every account, we only want the updates, not the actual content of the accounts
-        let rpc_account_info_config = RpcAccountInfoConfig {
-            commitment: self
-                .rpc_provider_config
-                .commitment()
-                .map(|commitment| CommitmentConfig { commitment }),
-            encoding: None,
-            data_slice: Some(UiDataSliceConfig {
-                offset: 0,
-                length: 0,
-            }),
-            min_context_slot: None,
-        };
-        // We'll store useful maps for each of the subscriptions
-        let mut streams = StreamMap::new();
-        let mut unsubscribes = HashMap::new();
         // Loop forever until we stop the worker
         loop {
             tokio::select! {
                 // When we receive a message to start monitoring an account
-                Some(account) = self.monitoring_request_receiver.recv() => {
-                    if unsubscribes.contains_key(&account) {
-                        continue;
-                    }
-                    info!("Account monitoring started: {}", account);
-                    let (stream, unsubscribe) = pubsub_client
-                        .account_subscribe(&account, Some(rpc_account_info_config.clone()))
-                        .await
-                        .map_err(RemoteAccountUpdatesWorkerError::PubsubClientError)?;
-                    streams.insert(account, stream);
-                    unsubscribes.insert(account, unsubscribe);
-                }
-                // When we receive an update from any account subscriptions
-                Some((account, update)) = streams.next() => {
-                    let current_update_slot = update.context.slot;
-                    info!(
-                        "Account update received: {}, in slot: {}",
-                        account, current_update_slot
-                    );
-                    match self.last_known_update_slots
-                        .write()
-                        .expect("RwLock of RemoteAccountUpdatesWorker.last_known_update_slots poisoned")
-                        .entry(account)
-                    {
-                        Entry::Vacant(entry) => {
-                            entry.insert(current_update_slot);
-                        }
-                        Entry::Occupied(mut entry) => {
-                            *entry.get_mut() = max(*entry.get(), current_update_slot);
-                        }
-                    }
+                Some(pubkey) = self.monitoring_request_receiver.recv() => {
                 }
                 // When we want to stop the worker (it was cancelled)
                 _ = cancellation_token.cancelled() => {
@@ -125,13 +80,25 @@ impl RemoteAccountUpdatesWorker {
                 }
             }
         }
-        // Cleanup all subscriptions and wait for proper shutdown
-        for unsubscribe in unsubscribes.into_values() {
-            unsubscribe().await;
-        }
-        drop(streams);
-        pubsub_client.shutdown().await?;
         // Done
         Ok(())
+    }
+
+    async fn start_monitoring_shard_auto_refresh(
+        &self,
+        cancellation_token: CancellationToken,
+    ) {
+        loop {
+            tokio::select! {
+                // Periodically we refresh our subscriptions shards
+                _ = sleep(Duration::from_millis(10_000)) => {
+
+                }
+                // When we want to stop the worker (it was cancelled)
+                _ = cancellation_token.cancelled() => {
+                    break;
+                }
+            }
+        }
     }
 }
