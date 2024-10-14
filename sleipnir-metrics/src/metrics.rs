e907d3872c9255a1d52a2551818cd7cd33c56f7e
Thorsten Lorenz <thlorenz@gmx.de>
2024-10-14 15:58:25 +0700

    feat: tracking transaction related metrics

--- sleipnir-metrics/src/metrics.rs
+++ sleipnir-metrics/src/metrics.rs
@@ -1,12 +1,25 @@
 use std::sync::Once;
 
-use prometheus::{IntCounter, Registry};
+use prometheus::{IntCounter, IntCounterVec, Opts, Registry};
 
 lazy_static::lazy_static! {
     pub(crate) static ref REGISTRY: Registry = Registry::new();
 
-    pub static ref SLOT_COUNTER: IntCounter = IntCounter::new(
-        "mbv_slot_counter", "Slot Counter",
+    pub static ref SLOT_COUNT: IntCounter = IntCounter::new(
+        "mbv_slot_count", "Slot Count",
+    ).unwrap();
+
+    pub static ref TRANSACTION_VEC_COUNT: IntCounterVec = IntCounterVec::new(
+        Opts::new("mbv_transaction_count", "Transaction Count"),
+        &["outcome"],
+    ).unwrap();
+
+    pub static ref EXECUTED_UNITS_COUNT: IntCounter = IntCounter::new(
+        "mbv_executed_units_count", "Executed Units (CU) Count",
+    ).unwrap();
+
+    pub static ref FEE_COUNT: IntCounter = IntCounter::new(
+        "mbv_fee_count", "Fee Count",
     ).unwrap();
 }
 
@@ -20,10 +33,29 @@ pub(crate) fn register() {
                     .expect("collector can't be registered");
             };
         }
-        register!(SLOT_COUNTER);
+        register!(SLOT_COUNT);
+        register!(TRANSACTION_VEC_COUNT);
+        register!(EXECUTED_UNITS_COUNT);
+        register!(FEE_COUNT);
     });
 }
 
 pub fn inc_slot() {
-    SLOT_COUNTER.inc();
+    SLOT_COUNT.inc();
+}
+
+pub fn inc_transaction(is_ok: bool) {
+    if is_ok {
+        TRANSACTION_VEC_COUNT.with_label_values(&["success"]).inc();
+    } else {
+        TRANSACTION_VEC_COUNT.with_label_values(&["error"]).inc();
+    }
+}
+
+pub fn inc_executed_units(executed_units: u64) {
+    EXECUTED_UNITS_COUNT.inc_by(executed_units);
+}
+
+pub fn inc_fee(fee: u64) {
+    FEE_COUNT.inc_by(fee);
 }
