db04ae14dd0a82c99460016bf58f97967bf59156
Vincent Brunet <vincent.brunet.us@gmail.com>
2024-10-16 13:10:22 +0900

    nit-pr

--- sleipnir-config/src/ledger.rs
+++ sleipnir-config/src/ledger.rs
@@ -1,31 +1,25 @@
 use serde::{Deserialize, Serialize};
 
+use crate::helpers::serde_defaults::bool_true;
+
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct LedgerConfig {
     /// If a previous ledger is found it is removed before starting the validator
     /// This can be disabled by setting [Self::reset] to `false`.
-    #[serde(default = "default_reset")]
+    #[serde(default = "bool_true")]
     pub reset: bool,
     // The file system path onto which the ledger should be written at
     // If left empty it will be auto-generated to a temporary folder
-    #[serde(default = "default_path")]
+    #[serde(default)]
     pub path: Option<String>,
-}
-
-fn default_reset() -> bool {
-    true
-}
-
-fn default_path() -> Option<String> {
-    None
 }
 
 impl Default for LedgerConfig {
     fn default() -> Self {
         Self {
-            reset: default_reset(),
-            path: default_path(),
+            reset: bool_true(),
+            path: Default::default(),
         }
     }
 }
