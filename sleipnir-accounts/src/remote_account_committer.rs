3bec2ac97905b5613cf108a78ee0d0d0e7808956
Gabriele Picco <gabriele@magicblock.gg>
2024-11-17 17:51:26 +0100

    fix: increase compute budget

--- sleipnir-accounts/src/remote_account_committer.rs
+++ sleipnir-accounts/src/remote_account_committer.rs
@@ -371,9 +371,9 @@ impl RemoteAccountCommitter {
     ) -> (Instruction, Instruction) {
         // TODO(thlorenz): We may need to consider account size as well since
         // the account is copied which could affect CUs
-        const BASE_COMPUTE_BUDGET: u32 = 50_000;
+        const BASE_COMPUTE_BUDGET: u32 = 60_000;
         const COMPUTE_BUDGET_PER_COMMITTEE: u32 = 40_000;
-        const COMPUTE_BUDGET_PER_UNDELEGATION: u32 = 40_000;
+        const COMPUTE_BUDGET_PER_UNDELEGATION: u32 = 45_000;
 
         let compute_budget = BASE_COMPUTE_BUDGET
             + (COMPUTE_BUDGET_PER_COMMITTEE * committee_count)
