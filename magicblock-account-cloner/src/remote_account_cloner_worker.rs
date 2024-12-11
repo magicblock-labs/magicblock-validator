908eb356f1b0e499a0177a5bd9af92af94bf4510
Thorsten Lorenz <thlorenz@gmx.de>
2024-12-11 09:43:46 +0700

    feat: initial concurrent hydrate-cloning

--- magicblock-account-cloner/src/remote_account_cloner_worker.rs
+++ magicblock-account-cloner/src/remote_account_cloner_worker.rs
@@ -8,7 +8,10 @@ use std::{
 use conjunto_transwise::{
     AccountChainSnapshotShared, AccountChainState, DelegationRecord,
 };
-use futures_util::future::join_all;
+use futures_util::{
+    future::join_all,
+    stream::{self, StreamExt, TryStreamExt},
+};
 use log::*;
 use magicblock_account_dumper::AccountDumper;
 use magicblock_account_fetcher::AccountFetcher;
@@ -246,30 +249,42 @@ where
             .map(|(pubkey, acc)| (pubkey, *acc.owner()))
             .collect::<HashSet<_>>();
 
-        for (pubkey, owner) in account_keys {
-            let res = self
-                .do_clone_and_update_cache(
-                    &pubkey,
-                    ValidatorStage::Hydrating {
-                        validator_identity: self.validator_identity,
-                        account_owner: owner,
-                    },
-                )
-                .await;
-            match res {
-                Ok(output) => {
-                    debug!("Cloned '{}': {:?}", pubkey, output);
+        debug!("Hydrating {} accounts", account_keys.len());
+        let stream = stream::iter(account_keys);
+        // NOTE: depending on the RPC provider we may get rate limited if we request
+        // account states at a too high rate.
+        // I confirmed the the following concurrency working fine:
+        //   Solana Mainnet: 10
+        //   Helius: 20
+        // If the optimal rate here is desired we might make this configurable in the
+        // future.
+        stream
+            .for_each_concurrent(10, |(pubkey, owner)| async move {
+                debug!("Hydrating '{}'", pubkey);
+                let res = self
+                    .do_clone_and_update_cache(
+                        &pubkey,
+                        ValidatorStage::Hydrating {
+                            validator_identity: self.validator_identity,
+                            account_owner: owner,
+                        },
+                    )
+                    .await;
+                match res {
+                    Ok(output) => {
+                        debug!("Cloned '{}': {:?}", pubkey, output);
+                    }
+                    Err(err) => {
+                        // TODO: @@@ what to do here?
+                        // Even empty accounts should clone fine with 0 lamports which would
+                        // cover the case that the account was removed from chain in the meantime
+                        // Thus if we encounter an error our validator cannot restore a proper
+                        // clone state and we should probably shut it down.
+                        error!("Failed to clone {} ('{:?}')", pubkey, err);
+                    }
                 }
-                Err(err) => {
-                    // TODO: @@@ what to do here?
-                    // Even empty accounts should clone fine with 0 lamports which would
-                    // cover the case that the account was removed from chain in the meantime
-                    // Thus if we encounter an error our validator cannot restore a proper
-                    // clone state and we should probably shut it down.
-                    error!("Failed to clone {} ('{:?}')", pubkey, err);
-                }
-            }
-        }
+            })
+            .await;
     }
 
     async fn do_clone_or_use_cache(
