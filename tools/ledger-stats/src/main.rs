91f960a077c9007e05b0dc9b1d7632ce6fd76ffc
Thorsten Lorenz <thlorenz@gmx.de>
2024-12-11 15:13:15 +0700

    feat: complete counts command

--- tools/ledger-stats/src/main.rs
+++ tools/ledger-stats/src/main.rs
@@ -1,34 +1,44 @@
 use magicblock_ledger::Ledger;
 use std::path::PathBuf;
+use std::str::FromStr;
 use structopt::StructOpt;
-use tabular::{Row, Table};
+
+mod counts;
+
+#[derive(Debug, Default, StructOpt)]
+enum Command {
+    #[default]
+    Counts,
+}
+
+impl FromStr for Command {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use Command::*;
+        match s.to_lowercase().as_str() {
+            "counts" => Ok(Counts),
+            _ => Err(format!("Unknown format: {}", s)),
+        }
+    }
+}
 
 #[derive(StructOpt)]
 struct Cli {
-    /// The path to the ledger
+    #[structopt(subcommand)]
+    command: Option<Command>,
+
     #[structopt(parse(from_os_str))]
     ledger_path: PathBuf,
-
-    /// The format to display the output
-    #[structopt(long, default_value = "counts")]
-    format: String,
 }
 
 fn main() {
     let args = Cli::from_args();
 
-    let ledger = Ledger::open(&args.ledger_path).expect("Failed to open ledger");
+    let ledger =
+        Ledger::open(&args.ledger_path).expect("Failed to open ledger");
 
-    let block_times_count = ledger
-        .count_block_times()
-        .expect("Failed to count block times");
-
-    let mut table = Table::new("{:<}  {:<}");
-    table.add_row(Row::new().with_cell("Column").with_cell("Count"));
-    table.add_row(
-        Row::new()
-            .with_cell("BlockTimes")
-            .with_cell(block_times_count),
-    );
-    println!("{}", table);
+    match args.command.unwrap_or_default() {
+        Command::Counts => counts::print_counts(&ledger),
+    }
 }
