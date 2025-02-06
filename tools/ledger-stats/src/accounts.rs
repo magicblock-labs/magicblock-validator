138a0b8874c1bd4ad9c52b2bacf8bb1f86b39ba4
Thorsten Lorenz <thlorenz@gmx.de>
2025-02-05 19:38:34 -0700

    fix: ledger-stats accounts

--- tools/ledger-stats/src/accounts.rs
+++ tools/ledger-stats/src/accounts.rs
@@ -6,7 +6,7 @@ use solana_sdk::{account::ReadableAccount, clock::Epoch, pubkey::Pubkey};
 use structopt::StructOpt;
 use tabular::{Row, Table};
 
-use crate::utils::accounts_storage_from_ledger;
+use crate::utils::{accounts_storage_from_ledger, all_accounts};
 
 // -----------------
 // SortAccounts
@@ -91,19 +91,19 @@ impl FilterAccounts {
 // -----------------
 // AccountInfo
 // -----------------
-struct AccountInfo<'a> {
+struct AccountInfo {
     /// Pubkey of the account
-    pub pubkey: &'a Pubkey,
+    pub pubkey: Pubkey,
     /// lamports in the account
     pub lamports: u64,
     /// the epoch at which this account will next owe rent
     pub rent_epoch: Epoch,
     /// the program that owns this account. If executable, the program that loads this account.
-    pub owner: &'a Pubkey,
+    pub owner: Pubkey,
     /// this account's data contains a loaded program (and is now read-only)
     pub executable: bool,
     /// the data in this account
-    pub data: &'a [u8],
+    pub data: Vec<u8>,
 }
 
 pub fn print_accounts(
@@ -115,117 +115,108 @@ pub fn print_accounts(
     count: bool,
 ) {
     let (storage, slot) = accounts_storage_from_ledger(ledger);
-    //storage.accounts
+    let mut accounts = {
+        let all = all_accounts(&storage, |acc_meta| AccountInfo {
+            pubkey: *acc_meta.pubkey(),
+            lamports: acc_meta.lamports(),
+            rent_epoch: acc_meta.rent_epoch(),
+            owner: *acc_meta.owner(),
+            executable: acc_meta.executable(),
+            data: acc_meta.data().to_vec(),
+        });
+        all.into_iter()
+            .filter(|acc| {
+                if !owner.map_or(true, |owner| acc.owner.eq(&owner)) {
+                    return false;
+                }
+                if filters.contains(&FilterAccounts::Executable)
+                    && !acc.executable
+                {
+                    return false;
+                }
+                if filters.contains(&FilterAccounts::NonExecutable)
+                    && acc.executable
+                {
+                    return false;
+                }
+                if filters.contains(&FilterAccounts::OnCurve)
+                    && !acc.pubkey.is_on_curve()
+                {
+                    return false;
+                }
+                if filters.contains(&FilterAccounts::OffCurve)
+                    && acc.pubkey.is_on_curve()
+                {
+                    return false;
+                }
 
-    todo!()
-    //let mut accounts = {
-    //    // TODO all_accounts impl
-    //    //let all = storage.all_accounts();
-    //    let all = vec![];
-    //    all.into_iter()
-    //        .filter(|acc| {
-    //            if !owner.map_or(true, |owner| acc.owner().eq(&owner)) {
-    //                return false;
-    //            }
-    //            if filters.contains(&FilterAccounts::Executable)
-    //                && !acc.executable()
-    //            {
-    //                return false;
-    //            }
-    //            if filters.contains(&FilterAccounts::NonExecutable)
-    //                && acc.executable()
-    //            {
-    //                return false;
-    //            }
-    //            if filters.contains(&FilterAccounts::OnCurve)
-    //                && !acc.pubkey().is_on_curve()
-    //            {
-    //                return false;
-    //            }
-    //            if filters.contains(&FilterAccounts::OffCurve)
-    //                && acc.pubkey().is_on_curve()
-    //            {
-    //                return false;
-    //            }
-    //
-    //            true
-    //        })
-    //        .collect::<Vec<_>>()
-    //};
-    //accounts.sort_by(|a, b| {
-    //    use SortAccounts::*;
-    //    match sort {
-    //        Pubkey => a.pubkey().cmp(b.pubkey()),
-    //        Owner => a.owner().cmp(b.owner()),
-    //        Lamports => a.lamports().cmp(&b.lamports()),
-    //        Executable => a.executable().cmp(&b.executable()),
-    //        DataLen => a.data().len().cmp(&b.data().len()),
-    //        RentEpoch => a.rent_epoch().cmp(&b.rent_epoch()),
-    //    }
-    //});
-    //
-    //if count {
-    //    if let Some(owner) = owner {
-    //        println!(
-    //            "Total accounts at slot {} owned by '{}': {}",
-    //            slot,
-    //            owner,
-    //            accounts.len()
-    //        );
-    //    } else {
-    //        println!("Total accounts at slot {}: {}", slot, accounts.len());
-    //    }
-    //    return;
-    //}
-    //
-    //let table_alignment = if print_rent_epoch {
-    //    "{:<}  {:<}  {:>}  {:<}  {:>}  {:<}  {:>}"
-    //} else {
-    //    "{:<}  {:<}  {:>}  {:<}  {:>}  {:<}"
-    //};
-    //let mut table = Table::new(table_alignment);
-    //let mut row = Row::new()
-    //    .with_cell("Pubkey")
-    //    .with_cell("Owner")
-    //    .with_cell("Lamports")
-    //    .with_cell("Executable")
-    //    .with_cell("Data(Bytes)")
-    //    .with_cell("Curve");
-    //if print_rent_epoch {
-    //    row.add_cell("Rent Epoch");
-    //}
-    //table.add_row(row);
-    //
-    //fn add_row(table: &mut Table, meta: AccountInfo, include_rent_epoch: bool) {
-    //    let oncurve = meta.pubkey.is_on_curve();
-    //    let mut row = Row::new()
-    //        .with_cell(meta.pubkey.to_string())
-    //        .with_cell(meta.owner.to_string())
-    //        .with_cell(meta.lamports.to_formatted_string(&Locale::en))
-    //        .with_cell(meta.executable)
-    //        .with_cell(meta.data.len())
-    //        .with_cell(if oncurve { "On" } else { "Off" });
-    //    if include_rent_epoch {
-    //        row.add_cell(meta.rent_epoch.to_formatted_string(&Locale::en));
-    //    }
-    //    table.add_row(row);
-    //}
-    //
-    //for acc in accounts {
-    //    add_row(
-    //        &mut table,
-    //        AccountInfo {
-    //            pubkey: acc.pubkey(),
-    //            lamports: acc.lamports(),
-    //            rent_epoch: acc.rent_epoch(),
-    //            owner: acc.owner(),
-    //            executable: acc.executable(),
-    //            data: acc.data(),
-    //        },
-    //        print_rent_epoch,
-    //    );
-    //}
-    //
-    //println!("Accounts at slot {}", slot);
-    //println!("{}", table);
+                true
+            })
+            .collect::<Vec<_>>()
+    };
+    accounts.sort_by(|a, b| {
+        use SortAccounts::*;
+        match sort {
+            Pubkey => a.pubkey.cmp(&b.pubkey),
+            Owner => a.owner.cmp(&b.owner),
+            Lamports => a.lamports.cmp(&b.lamports),
+            Executable => a.executable.cmp(&b.executable),
+            DataLen => a.data.len().cmp(&b.data.len()),
+            RentEpoch => a.rent_epoch.cmp(&b.rent_epoch),
+        }
+    });
+
+    if count {
+        if let Some(owner) = owner {
+            println!(
+                "Total accounts at slot {} owned by '{}': {}",
+                slot,
+                owner,
+                accounts.len()
+            );
+        } else {
+            println!("Total accounts at slot {}: {}", slot, accounts.len());
+        }
+        return;
+    }
+
+    let table_alignment = if print_rent_epoch {
+        "{:<}  {:<}  {:>}  {:<}  {:>}  {:<}  {:>}"
+    } else {
+        "{:<}  {:<}  {:>}  {:<}  {:>}  {:<}"
+    };
+    let mut table = Table::new(table_alignment);
+    let mut row = Row::new()
+        .with_cell("Pubkey")
+        .with_cell("Owner")
+        .with_cell("Lamports")
+        .with_cell("Executable")
+        .with_cell("Data(Bytes)")
+        .with_cell("Curve");
+    if print_rent_epoch {
+        row.add_cell("Rent Epoch");
+    }
+    table.add_row(row);
+
+    fn add_row(table: &mut Table, meta: AccountInfo, include_rent_epoch: bool) {
+        let oncurve = meta.pubkey.is_on_curve();
+        let mut row = Row::new()
+            .with_cell(meta.pubkey.to_string())
+            .with_cell(meta.owner.to_string())
+            .with_cell(meta.lamports.to_formatted_string(&Locale::en))
+            .with_cell(meta.executable)
+            .with_cell(meta.data.len())
+            .with_cell(if oncurve { "On" } else { "Off" });
+        if include_rent_epoch {
+            row.add_cell(meta.rent_epoch.to_formatted_string(&Locale::en));
+        }
+        table.add_row(row);
+    }
+
+    for acc in accounts {
+        add_row(&mut table, acc, print_rent_epoch);
+    }
+
+    println!("Accounts at slot {}", slot);
+    println!("{}", table);
 }
