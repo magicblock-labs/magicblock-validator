aacab7a657d23b430907d1c4c2c57e440cf3fed7
Babur Makhmudov <bmuddha13@gmail.com>
2025-05-23 21:38:50 +0400

    feat: implement basic subscription handlers

--- magicblock-pubsub/src/handler/common.rs
+++ magicblock-pubsub/src/handler/common.rs
@@ -1,14 +1,7 @@
-use geyser_grpc_proto::{geyser, tonic::Status};
 use jsonrpc_pubsub::Sink;
-use log::*;
+use magicblock_geyser_plugin::types::SubscriptionsDb;
 use serde::{Deserialize, Serialize};
-use solana_account_decoder::{UiAccount, UiAccountEncoding};
-
-use crate::{
-    conversions::{slot_from_update, subscribe_update_try_into_ui_account},
-    errors::sink_notify_error,
-    types::{AccountDataConfig, ResponseWithSubscriptionId},
-};
+use solana_account_decoder::UiAccount;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct UiAccountWithPubkey {
@@ -16,70 +9,9 @@ pub struct UiAccountWithPubkey {
     pub account: UiAccount,
 }
 
-/// Handles geyser update for account and program subscriptions.
-/// Returns true if subscription has ended.
-pub fn handle_account_geyser_update(
-    sink: &Sink,
-    subid: u64,
-    update: Result<geyser::SubscribeUpdate, Status>,
-    params: AccountDataConfig,
-    include_pubkey: bool,
-) -> bool {
-    match update {
-        Ok(update) => {
-            debug!("Received geyser update: {:?}", update);
-
-            let slot = slot_from_update(&update).unwrap_or(0);
-
-            let encoding = params.encoding.unwrap_or(UiAccountEncoding::Base58);
-            let (pubkey, ui_account) =
-                match subscribe_update_try_into_ui_account(
-                    update,
-                    encoding,
-                    params.data_slice_config,
-                ) {
-                    Ok(Some(ui_account)) => ui_account,
-                    Ok(None) => {
-                        debug!("No account data in update, skipping.");
-                        return false;
-                    }
-                    Err(err) => {
-                        let msg = format!(
-                            "Failed to convert update to UiAccount: {:?}",
-                            err
-                        );
-                        let failed_to_notify = sink_notify_error(sink, msg);
-                        return failed_to_notify;
-                    }
-                };
-            let notify_res = if include_pubkey {
-                let res = ResponseWithSubscriptionId::new(
-                    UiAccountWithPubkey {
-                        pubkey: pubkey.to_string(),
-                        account: ui_account,
-                    },
-                    slot,
-                    subid,
-                );
-                debug!("Sending response: {:?}", res);
-                sink.notify(res.into_params_map())
-            } else {
-                let res =
-                    ResponseWithSubscriptionId::new(ui_account, slot, subid);
-                debug!("Sending response: {:?}", res);
-                sink.notify(res.into_params_map())
-            };
-
-            if let Err(err) = notify_res {
-                debug!("Subscription has ended, finishing {:?}.", err);
-                true
-            } else {
-                false
-            }
-        }
-        Err(status) => sink_notify_error(
-            sink,
-            format!("Failed to receive signature update: {:?}", status),
-        ),
-    }
+pub struct UpdateHandler<B, C> {
+    pub sink: Sink,
+    pub builder: B,
+    pub subscriptions_db: SubscriptionsDb,
+    pub cleanup: C,
 }
