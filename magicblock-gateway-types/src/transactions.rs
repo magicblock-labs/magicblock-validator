31774075ae45a80875087bb2d818116aac9e920c
Babur Makhmudov <31780624+bmuddha@users.noreply.github.com>
2025-07-28 13:28:04 +0400

    Bmuddha/rpc/subscriptions db (#480)

--- magicblock-gateway-types/src/transactions.rs
+++ magicblock-gateway-types/src/transactions.rs
@@ -1,4 +1,5 @@
 use solana_message::inner_instruction::InnerInstructions;
+use solana_pubkey::Pubkey;
 use solana_signature::Signature;
 use solana_transaction::versioned::VersionedTransaction;
 use solana_transaction_context::{TransactionAccount, TransactionReturnData};
@@ -7,30 +8,7 @@ use tokio::sync::{
     oneshot,
 };
 
-pub struct TransactionStatus {
-    pub signature: Signature,
-    pub success: bool,
-}
-
-pub struct ProcessableTransaction {
-    pub transaction: VersionedTransaction,
-    pub simulate: bool,
-    pub result_tx: Option<oneshot::Sender<TransactionResult>>,
-}
-
-pub enum TransactionProcessingResult {
-    Execution(TransactionResult),
-    Simulation(Box<TransactionSimulationResult>),
-}
-
-pub struct TransactionSimulationResult {
-    pub result: TransactionResult,
-    pub logs: Vec<String>,
-    pub post_simulation_accounts: Vec<TransactionAccount>,
-    pub units_consumed: u64,
-    pub return_data: Option<TransactionReturnData>,
-    pub inner_instructions: Option<Vec<InnerInstructions>>,
-}
+pub use solana_transaction_error::TransactionError;
 
 pub type TxnStatusRx = Receiver<TransactionStatus>;
 pub type TxnStatusTx = Sender<TransactionStatus>;
@@ -42,3 +20,34 @@ pub type TxnResultRx = Receiver<TransactionProcessingResult>;
 pub type TxnResultTx = Sender<TransactionProcessingResult>;
 
 pub type TransactionResult = solana_transaction_error::TransactionResult<()>;
+
+pub struct TransactionStatus {
+    pub signature: Signature,
+    pub result: TransactionProcessingResult,
+}
+
+pub struct ProcessableTransaction {
+    pub transaction: VersionedTransaction,
+    pub simulate: bool,
+    pub result_tx: Option<oneshot::Sender<TransactionResult>>,
+}
+
+pub enum TransactionProcessingResult {
+    Execution(TransactionExecutionResult),
+    Simulation(TransactionSimulationResult),
+}
+
+pub struct TransactionExecutionResult {
+    pub result: TransactionResult,
+    pub accounts: Box<[Pubkey]>,
+    pub logs: Box<[String]>,
+}
+
+pub struct TransactionSimulationResult {
+    pub result: TransactionResult,
+    pub logs: Box<[String]>,
+    pub post_simulation_accounts: Box<[TransactionAccount]>,
+    pub units_consumed: u64,
+    pub return_data: Option<Box<TransactionReturnData>>,
+    pub inner_instructions: Option<Box<[InnerInstructions]>>,
+}
---
+++ magicblock-gateway/src/requests/websocket/mod.rs
@@ -0,0 +1,4 @@
+pub(crate) mod account_subscribe;
+pub(crate) mod log_subscribe;
+pub(crate) mod program_subscribe;
+pub(crate) mod slot_subscribe;
