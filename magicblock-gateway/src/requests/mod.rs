ce327d478165584d8f08e27d0efae9fa8b855d8a
Babur Makhmudov <31780624+bmuddha@users.noreply.github.com>
2025-07-30 19:27:33 +0400

    feat: implemented request dispatchers (#483)

--- magicblock-gateway/src/requests/mod.rs
+++ magicblock-gateway/src/requests/mod.rs
@@ -2,9 +2,6 @@ use std::fmt::Display;
 
 use json::{Array, Value};
 
-pub(crate) mod http;
-pub(crate) mod websocket;
-
 #[derive(json::Deserialize)]
 pub(crate) struct JsonRequest {
     pub(crate) id: Value,
@@ -45,3 +42,26 @@ impl Display for JsonRpcMethod {
         write!(f, "{self:?}")
     }
 }
+
+macro_rules! parse_params {
+    ($input: expr, $ty1: ty) => {
+        $input.pop().and_then(|v| json::from_value::<$ty1>(&v).ok())
+    };
+    ($input: expr, $ty1: ty, $ty2: ty) => {{
+        $input.reverse();
+        (parse_params!($input, $ty1), parse_params!($input, $ty2))
+    }};
+    ($input: expr, $ty1: ty, $ty2: ty, $ty3: ty) => {{
+        $input.reverse();
+        (
+            parse_params!($input, $ty1),
+            parse_params!($input, $ty2),
+            parse_params!($input, $ty3),
+        )
+    }};
+}
+
+pub(crate) mod http;
+pub(crate) mod params;
+pub(crate) mod payload;
+pub(crate) mod websocket;
