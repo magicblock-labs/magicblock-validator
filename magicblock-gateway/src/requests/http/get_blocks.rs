2e514709a19e95a8dc74d209f5d0eb28adf33497
Babur Makhmudov <bmuddha13@gmail.com>
2025-08-04 17:03:21 +0400

    refactor: added macro for converting None to invalid params

--- magicblock-gateway/src/requests/http/get_blocks.rs
+++ magicblock-gateway/src/requests/http/get_blocks.rs
@@ -6,13 +6,14 @@ impl HttpDispatcher {
         request: &mut JsonRequest,
     ) -> HandlerResult {
         let (start, end) = parse_params!(request.params()?, Slot, Slot);
-        let start = start
-            .ok_or_else(|| RpcError::invalid_params("missing start slot"))?;
+        let start = some_or_err!(start, "start slot");
         let slot = self.accountsdb.slot();
         let end = end.map(|end| end.min(slot)).unwrap_or(slot);
-        (start < end).then_some(()).ok_or_else(|| {
-            RpcError::invalid_params("start slot is greater than the end slot")
-        })?;
+        if start < end {
+            Err(RpcError::invalid_params(
+                "start slot is greater than the end slot",
+            ))?;
+        };
         let range = (start..=end).collect::<Vec<Slot>>();
         Ok(ResponsePayload::encode_no_context(&request.id, range))
     }
--- magicblock-gateway/src/requests/http/is_blockhash_valid.rs
+++ magicblock-gateway/src/requests/http/is_blockhash_valid.rs
@@ -6,9 +6,7 @@ impl HttpDispatcher {
         request: &mut JsonRequest,
     ) -> HandlerResult {
         let blockhash = parse_params!(request.params()?, Serde32Bytes);
-        let blockhash = blockhash.map(Into::into).ok_or_else(|| {
-            RpcError::invalid_params("missing or invalid blockhash")
-        })?;
+        let blockhash = some_or_err!(blockhash);
         let valid = self.blocks.contains(&blockhash);
         let slot = self.accountsdb.slot();
         Ok(ResponsePayload::encode(&request.id, valid, slot))
