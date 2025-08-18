e030e3d80a69fb2e13f92286b893c4623dc3888b
Dodecahedr0x <hexadecifish@gmail.com>
2025-08-18 13:54:05 +0200

    fix: next slot ephem

--- test-integration/test-ledger-restore/src/resume_strategies.rs
+++ test-integration/test-ledger-restore/src/resume_strategies.rs
@@ -41,7 +41,7 @@ pub fn write(ledger_path: &Path, kp: &mut Keypair) -> (Child, u64, Signature) {
     );
 
     // Wait slot 1 otherwise we might be unable to fetch the transaction status
-    expect!(ctx.wait_for_next_slot_chain(), validator);
+    expect!(ctx.wait_for_next_slot_ephem(), validator);
 
     let signature =
         expect!(ctx.airdrop_ephem(&kp.pubkey(), 1_111_111), validator);
