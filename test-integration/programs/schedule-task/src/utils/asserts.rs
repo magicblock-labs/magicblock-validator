d04146931c3e65ff22eec657faf15665669e5aae
Dodecahedr0x <hexadecifish@gmail.com>
2025-08-20 00:27:51 +0200

    chore: remove dead code

--- test-integration/programs/schedule-task/src/utils/asserts.rs
+++ test-integration/programs/schedule-task/src/utils/asserts.rs
@@ -1,7 +1,5 @@
-#![allow(unused)]
 use solana_program::{
-    account_info::AccountInfo, entrypoint::ProgramResult, msg,
-    program_error::ProgramError, pubkey::Pubkey,
+    entrypoint::ProgramResult, msg, program_error::ProgramError, pubkey::Pubkey,
 };
 
 pub fn assert_keys_equal<F: FnOnce() -> String>(
@@ -13,41 +11,6 @@ pub fn assert_keys_equal<F: FnOnce() -> String>(
         msg!("Err: {}", get_msg());
         msg!("Err: provided {} expected {}", provided_key, expected_key);
         Err(ProgramError::Custom(1))
-    } else {
-        Ok(())
-    }
-}
-
-pub fn assert_is_signer(
-    account: &AccountInfo,
-    account_label: &str,
-) -> ProgramResult {
-    if !account.is_signer {
-        msg!(
-            "Err: account '{}' ({}) should be signer",
-            account_label,
-            account.key
-        );
-        Err(ProgramError::MissingRequiredSignature)
-    } else {
-        Ok(())
-    }
-}
-
-pub fn assert_size(
-    account: &AccountInfo,
-    size: usize,
-    account_label: &str,
-) -> ProgramResult {
-    let account_size = account.try_data_len()?;
-    if account_size != size {
-        msg!(
-            "Err: account '{}' should have size {} but has {}",
-            account_label,
-            account_size,
-            size
-        );
-        Err(ProgramError::InvalidAccountData)
     } else {
         Ok(())
     }
