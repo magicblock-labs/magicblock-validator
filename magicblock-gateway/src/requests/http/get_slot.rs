d628dba12ddaebb36cb04a2cdb82616b6b0aab28
Babur Makhmudov <31780624+bmuddha@users.noreply.github.com>
2025-08-29 17:42:24 +0400

    fix: cleanup get_slot

--- magicblock-gateway/src/requests/http/get_slot.rs
+++ magicblock-gateway/src/requests/http/get_slot.rs
@@ -2,7 +2,7 @@ use super::prelude::*;
 
 impl HttpDispatcher {
     pub(crate) fn get_slot(&self, request: &JsonRequest) -> HandlerResult {
-        let slot = self.accountsdb.slot();
+        let slot = self.blocks.block_height();
         Ok(ResponsePayload::encode_no_context(&request.id, slot))
     }
 }
