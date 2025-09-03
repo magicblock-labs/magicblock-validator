52dea0f8266ca27d15abf8beb6702e3c59c0144c
Dodecahedr0x <hexadecifish@gmail.com>
2025-09-03 19:32:06 +0200

    fix: broken integration tests

--- test-integration/test-task-scheduler/tests/test_schedule_task.rs
+++ test-integration/test-task-scheduler/tests/test_schedule_task.rs
@@ -1,6 +1,7 @@
-use std::{path::PathBuf, thread::sleep};
+use std::thread::sleep;
 
-use integration_test_tools::IntegrationTestContext;
+use cleanass::{assert, assert_eq};
+use integration_test_tools::{expect, validator::cleanup};
 use magicblock_program::{ID as MAGIC_PROGRAM_ID, TASK_CONTEXT_PUBKEY};
 use magicblock_task_scheduler::SchedulerDatabase;
 use schedule_task_program::{
@@ -14,44 +15,57 @@ use solana_sdk::{
     native_token::LAMPORTS_PER_SOL, signature::Keypair, signer::Signer,
     transaction::Transaction,
 };
-use test_task_scheduler::DB_PATH;
+use test_task_scheduler::setup_validator;
 
 #[test]
 fn test_schedule_task() {
-    let ctx = IntegrationTestContext::try_new().unwrap();
+    let (temp_dir, mut validator, ctx) = setup_validator();
+    let db_path = SchedulerDatabase::path(temp_dir.path());
+
     let payer = Keypair::new();
     let (counter_pda, _) = Counter::pda(&payer.pubkey());
 
-    ctx.airdrop_chain(&payer.pubkey(), 10 * LAMPORTS_PER_SOL)
-        .unwrap();
+    expect!(
+        ctx.airdrop_chain(&payer.pubkey(), 10 * LAMPORTS_PER_SOL),
+        validator
+    );
 
     // Initialize the counter
-    ctx.try_chain_client()
-        .unwrap()
-        .send_transaction(&Transaction::new_signed_with_payer(
-            &[create_init_counter_ix(payer.pubkey())],
-            Some(&payer.pubkey()),
-            &[&payer],
-            ctx.try_chain_client()
-                .unwrap()
-                .get_latest_blockhash()
-                .unwrap(),
-        ))
-        .unwrap();
+    let blockhash = expect!(
+        ctx.try_chain_client().and_then(|client| client
+            .get_latest_blockhash()
+            .map_err(|e| anyhow::anyhow!(
+                "Failed to get latest blockhash: {}",
+                e
+            ))),
+        validator
+    );
+    expect!(
+        ctx.send_transaction_chain(
+            &mut Transaction::new_signed_with_payer(
+                &[create_init_counter_ix(payer.pubkey())],
+                Some(&payer.pubkey()),
+                &[&payer],
+                blockhash,
+            ),
+            &[&payer]
+        ),
+        validator
+    );
 
     // Delegate the counter to the ephem validator
-    ctx.try_chain_client()
-        .unwrap()
-        .send_transaction(&Transaction::new_signed_with_payer(
-            &[create_delegate_ix(payer.pubkey())],
-            Some(&payer.pubkey()),
-            &[&payer],
-            ctx.try_chain_client()
-                .unwrap()
-                .get_latest_blockhash()
-                .unwrap(),
-        ))
-        .unwrap();
+    expect!(
+        ctx.send_transaction_chain(
+            &mut Transaction::new_signed_with_payer(
+                &[create_delegate_ix(payer.pubkey())],
+                Some(&payer.pubkey()),
+                &[&payer],
+                blockhash,
+            ),
+            &[&payer]
+        ),
+        validator
+    );
 
     // Wait for account to be delegated
     sleep(std::time::Duration::from_secs(3));
@@ -60,90 +74,117 @@ fn test_schedule_task() {
     let task_id = 1;
     let period_millis = 100;
     let n_executions = 3;
-    ctx.try_ephem_client()
-        .unwrap()
-        .send_transaction(&Transaction::new_signed_with_payer(
-            &[create_schedule_task_ix(
-                payer.pubkey(),
-                TASK_CONTEXT_PUBKEY,
-                MAGIC_PROGRAM_ID,
-                task_id,
-                period_millis,
-                n_executions,
-                false,
-                false,
-            )],
-            Some(&payer.pubkey()),
-            &[&payer],
-            ctx.try_ephem_client()
-                .unwrap()
-                .get_latest_blockhash()
-                .unwrap(),
-        ))
-        .unwrap();
+    let ephem_blockhash = expect!(
+        ctx.try_ephem_client().and_then(|client| client
+            .get_latest_blockhash()
+            .map_err(|e| anyhow::anyhow!(
+                "Failed to get latest blockhash: {}",
+                e
+            ))),
+        validator
+    );
+
+    expect!(
+        ctx.send_transaction_ephem(
+            &mut Transaction::new_signed_with_payer(
+                &[create_schedule_task_ix(
+                    payer.pubkey(),
+                    TASK_CONTEXT_PUBKEY,
+                    MAGIC_PROGRAM_ID,
+                    task_id,
+                    period_millis,
+                    n_executions,
+                    false,
+                    false,
+                )],
+                Some(&payer.pubkey()),
+                &[&payer],
+                ephem_blockhash,
+            ),
+            &[&payer]
+        ),
+        validator
+    );
 
     // Wait for the task to be scheduled
     sleep(std::time::Duration::from_secs(3));
 
     // Check that the task was scheduled in the database
-    let db_path = PathBuf::from(DB_PATH);
-    let db = SchedulerDatabase::new(db_path).unwrap();
+    let db = expect!(SchedulerDatabase::new(db_path), validator);
 
-    let failed_scheduling = db.get_failed_scheduling_ids().unwrap();
+    let failed_scheduling = expect!(db.get_failed_scheduling_ids(), validator);
     assert_eq!(
         failed_scheduling.len(),
         0,
+        cleanup(&mut validator),
         "failed_scheduling: {:?}",
-        failed_scheduling
+        failed_scheduling,
     );
 
-    let failed_tasks = db.get_failed_task_ids().unwrap();
-    assert_eq!(failed_tasks.len(), 0, "failed_tasks: {:?}", failed_tasks);
+    let failed_tasks = expect!(db.get_failed_task_ids(), validator);
+    assert_eq!(
+        failed_tasks.len(),
+        0,
+        cleanup(&mut validator),
+        "failed_tasks: {:?}",
+        failed_tasks
+    );
 
-    let tasks = db.get_task_ids().unwrap();
-    assert_eq!(tasks.len(), 1);
+    let tasks = expect!(db.get_task_ids(), validator);
+    assert_eq!(tasks.len(), 1, cleanup(&mut validator));
 
-    let task = db.get_task(task_id).unwrap().unwrap();
-    assert_eq!(task.id, task_id);
-    assert_eq!(task.authority, payer.pubkey());
-    assert_eq!(task.execution_interval_millis, 100);
-    assert_eq!(task.executions_left, 0);
+    let task = expect!(
+        db.get_task(task_id)
+            .ok()
+            .flatten()
+            .ok_or(anyhow::anyhow!("Task not found")),
+        validator
+    );
+    assert_eq!(task.id, task_id, cleanup(&mut validator));
+    assert_eq!(task.authority, payer.pubkey(), cleanup(&mut validator));
+    assert_eq!(task.execution_interval_millis, 100, cleanup(&mut validator));
+    assert_eq!(task.executions_left, 0, cleanup(&mut validator));
 
     // Check that the counter was incremented
-    let counter_account = ctx
-        .try_ephem_client()
-        .unwrap()
-        .get_account(&counter_pda)
-        .unwrap();
-    let counter = Counter::try_decode(&counter_account.data).unwrap();
+    let counter_account = expect!(
+        ctx.try_ephem_client().and_then(|client| client
+            .get_account(&counter_pda)
+            .map_err(|e| anyhow::anyhow!("Failed to get account: {}", e))),
+        validator
+    );
+    let counter =
+        expect!(Counter::try_decode(&counter_account.data), validator);
     assert!(
         counter.count == n_executions,
+        cleanup(&mut validator),
         "counter.count: {}",
         counter.count
     );
 
     // Cancel the task
-    ctx.try_ephem_client()
-        .unwrap()
-        .send_transaction(&Transaction::new_signed_with_payer(
-            &[create_cancel_task_ix(
-                payer.pubkey(),
-                TASK_CONTEXT_PUBKEY,
-                MAGIC_PROGRAM_ID,
-                task_id,
-            )],
-            Some(&payer.pubkey()),
-            &[&payer],
-            ctx.try_ephem_client()
-                .unwrap()
-                .get_latest_blockhash()
-                .unwrap(),
-        ))
-        .unwrap();
+    expect!(
+        ctx.send_transaction_ephem(
+            &mut Transaction::new_signed_with_payer(
+                &[create_cancel_task_ix(
+                    payer.pubkey(),
+                    TASK_CONTEXT_PUBKEY,
+                    MAGIC_PROGRAM_ID,
+                    task_id,
+                )],
+                Some(&payer.pubkey()),
+                &[&payer],
+                ephem_blockhash,
+            ),
+            &[&payer]
+        ),
+        validator
+    );
 
     sleep(std::time::Duration::from_secs(3));
 
     // Check that the task was cancelled
-    let tasks = db.get_task_ids().unwrap();
-    assert_eq!(tasks.len(), 0);
+    let tasks = expect!(db.get_task_ids(), validator);
+    assert_eq!(tasks.len(), 0, cleanup(&mut validator));
+
+    cleanup(&mut validator);
 }
