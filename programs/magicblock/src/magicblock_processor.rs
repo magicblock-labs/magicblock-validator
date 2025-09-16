df4135c3665e698fb14a219796ae7d2c4167edef
Thorsten Lorenz <thlorenz@gmx.de>
2025-09-16 12:17:26 +0200

    feat: allow writing to executable data during account mutation

--- programs/magicblock/src/magicblock_processor.rs
+++ programs/magicblock/src/magicblock_processor.rs
@@ -17,50 +17,60 @@ declare_process_instruction!(
     Entrypoint,
     DEFAULT_COMPUTE_UNITS,
     |invoke_context| {
+        use MagicBlockInstruction::*;
+        let instruction = limited_deserialize(
+            invoke_context
+                .transaction_context
+                .get_current_instruction_context()?
+                .get_instruction_data(),
+        )?;
+        let disable_executable_check = matches!(instruction, ModifyAccounts(_));
+        // The below is necessary to avoid:
+        // 'instruction changed executable accounts data'
+        // writing data to and deploying a program account.
+        // NOTE: better to make this an instruction which does nothing but toggle
+        // this flag on and off around the instructions which need it off.
+        if disable_executable_check {
+            invoke_context
+                .transaction_context
+                .set_remove_accounts_executable_flag_checks(true);
+        }
         let transaction_context = &invoke_context.transaction_context;
         let instruction_context =
             transaction_context.get_current_instruction_context()?;
-        let instruction_data = instruction_context.get_instruction_data();
-        let instruction = limited_deserialize(instruction_data)?;
         let signers = instruction_context.get_signers(transaction_context)?;
 
         match instruction {
-            MagicBlockInstruction::ModifyAccounts(mut account_mods) => {
-                process_mutate_accounts(
-                    signers,
-                    invoke_context,
-                    transaction_context,
-                    &mut account_mods,
-                )
-            }
-            MagicBlockInstruction::ScheduleCommit => process_schedule_commit(
+            ModifyAccounts(mut account_mods) => process_mutate_accounts(
+                signers,
+                invoke_context,
+                transaction_context,
+                &mut account_mods,
+            ),
+            ScheduleCommit => process_schedule_commit(
                 signers,
                 invoke_context,
                 ProcessScheduleCommitOptions {
                     request_undelegation: false,
                 },
             ),
-            MagicBlockInstruction::ScheduleCommitAndUndelegate => {
-                process_schedule_commit(
-                    signers,
-                    invoke_context,
-                    ProcessScheduleCommitOptions {
-                        request_undelegation: true,
-                    },
-                )
-            }
-            MagicBlockInstruction::AcceptScheduleCommits => {
+            ScheduleCommitAndUndelegate => process_schedule_commit(
+                signers,
+                invoke_context,
+                ProcessScheduleCommitOptions {
+                    request_undelegation: true,
+                },
+            ),
+            AcceptScheduleCommits => {
                 process_accept_scheduled_commits(signers, invoke_context)
             }
-            MagicBlockInstruction::ScheduledCommitSent(id) => {
-                process_scheduled_commit_sent(
-                    signers,
-                    invoke_context,
-                    transaction_context,
-                    id,
-                )
-            }
-            MagicBlockInstruction::ScheduleBaseIntent(args) => {
+            ScheduledCommitSent(id) => process_scheduled_commit_sent(
+                signers,
+                invoke_context,
+                transaction_context,
+                id,
+            ),
+            ScheduleBaseIntent(args) => {
                 process_schedule_base_intent(signers, invoke_context, args)
             }
         }
