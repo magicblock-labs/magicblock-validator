a0a3a68d3e67d2edc1181d7c8dfb1efce76eb6b8
Dodecahedr0x <hexadecifish@gmail.com>
2025-09-19 15:35:22 +0200

    fix: set task context owner

--- magicblock-api/src/fund_account.rs
+++ magicblock-api/src/fund_account.rs
@@ -23,15 +23,17 @@ pub(crate) fn fund_account(bank: &Bank, pubkey: &Pubkey, lamports: u64) {
     fund_account_with_data(bank, pubkey, lamports, vec![]);
 }
 
-pub(crate) fn fund_account_with_data(
+pub(crate) fn fund_owned_account_with_data(
     bank: &Bank,
     pubkey: &Pubkey,
+    owner: Pubkey,
     lamports: u64,
     data: Vec<u8>,
 ) {
     if let Some(mut acc) = bank.get_account(pubkey) {
         acc.set_lamports(lamports);
         acc.set_data(data);
+        acc.set_owner(owner);
         bank.store_account(*pubkey, acc);
     } else {
         bank.store_account(
@@ -39,13 +41,28 @@ pub(crate) fn fund_account_with_data(
             Account {
                 lamports,
                 data,
-                owner: system_program::id(),
+                owner,
                 executable: false,
                 rent_epoch: Epoch::MAX,
             }
             .into(),
         );
     }
+}
+
+pub(crate) fn fund_account_with_data(
+    bank: &Bank,
+    pubkey: &Pubkey,
+    lamports: u64,
+    data: Vec<u8>,
+) {
+    fund_owned_account_with_data(
+        bank,
+        pubkey,
+        system_program::id(),
+        lamports,
+        data,
+    );
 }
 
 pub(crate) fn fund_validator_identity(bank: &Bank, validator_id: &Pubkey) {
@@ -85,9 +102,10 @@ pub(crate) fn fund_magic_context(bank: &Bank) {
 // Make rent-exempt to allow the PDA to receive rent
 pub(crate) fn fund_task_context(bank: &Bank) {
     // Initialize as an empty task context
-    fund_account_with_data(
+    fund_owned_account_with_data(
         bank,
         &TASK_CONTEXT_PUBKEY,
+        magicblock_program::ID,
         u64::MAX,
         TaskContext::ZERO.to_vec(),
     );
