0fe90b1387e6eeb540a550ac10f89951c498f970
Thorsten Lorenz <thlorenz@gmx.de>
2025-09-22 12:11:15 +0200

    chore: another test to triage lockup

--- test-integration/test-cloning/tests/04_escrow_transfer.rs
+++ test-integration/test-cloning/tests/04_escrow_transfer.rs
@@ -1,28 +1,13 @@
 use integration_test_tools::IntegrationTestContext;
 use log::*;
 use solana_sdk::{
-    native_token::LAMPORTS_PER_SOL, pubkey::Pubkey, signature::Keypair,
-    signer::Signer, system_instruction,
+    native_token::LAMPORTS_PER_SOL, signature::Keypair, signer::Signer,
+    system_instruction,
 };
 use test_kit::init_logger;
 
-fn init_and_delegate_flexi_counter(
-    ctx: &IntegrationTestContext,
-    counter_auth: &Keypair,
-) -> Pubkey {
-    use program_flexi_counter::{instruction::*, state::*};
-    ctx.airdrop_chain(&counter_auth.pubkey(), 5 * LAMPORTS_PER_SOL)
-        .expect("counter auth airdrop failed");
-    let init_counter_ix =
-        create_init_ix(counter_auth.pubkey(), "COUNTER".to_string());
-    let delegate_ix = create_delegate_ix(counter_auth.pubkey());
-    ctx.send_and_confirm_instructions_with_payer_chain(
-        &[init_counter_ix, delegate_ix],
-        counter_auth,
-    )
-    .unwrap();
-    FlexiCounter::pda(&counter_auth.pubkey()).0
-}
+use crate::utils::init_and_delegate_flexi_counter;
+mod utils;
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn test_transfer_from_escrow_to_delegated_account() {
