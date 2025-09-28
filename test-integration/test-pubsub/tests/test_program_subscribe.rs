8a2eae57132a378918a2575f6afc8667949f5576
Thorsten Lorenz <thlorenz@gmx.de>
2025-09-28 21:42:21 +0100

    chore: fix slot + program subscribe tests - cancel is brittle in all

--- test-integration/test-pubsub/tests/test_program_subscribe.rs
+++ test-integration/test-pubsub/tests/test_program_subscribe.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use futures::StreamExt;
 use solana_sdk::{
     native_token::LAMPORTS_PER_SOL, pubkey::Pubkey, signer::Signer,
@@ -15,21 +17,36 @@ async fn test_program_subscribe() {
 
     const TRANSFER_AMOUNT: u64 = 10_000;
     env.transfer(TRANSFER_AMOUNT);
+    let expected_lamports1 = LAMPORTS_PER_SOL - TRANSFER_AMOUNT;
+    let expected_lamports2 = LAMPORTS_PER_SOL + TRANSFER_AMOUNT;
     for _ in 0..2 {
-        let update = rx
-            .next()
+        // We ignore all updates for the accounts like them being cloned
+        // until we see the balance changes
+        let update = loop {
+            let update = tokio::time::timeout(
+                Duration::from_millis(100),
+                rx.next(),
+            )
             .await
+            .expect("timeout waiting for program update")
             .expect("failed to receive accounts update after balance change");
+
+            if (update.value.pubkey == env.account1.pubkey().to_string()
+                && update.value.account.lamports == expected_lamports1)
+                || (update.value.pubkey == env.account2.pubkey().to_string()
+                    && update.value.account.lamports == expected_lamports2)
+            {
+                break update;
+            }
+        };
         if update.value.pubkey == env.account1.pubkey().to_string() {
             assert_eq!(
-                update.value.account.lamports,
-                LAMPORTS_PER_SOL - TRANSFER_AMOUNT,
+                update.value.account.lamports, expected_lamports1,
                 "account 1 should have its balance decreased"
             );
         } else {
             assert_eq!(
-                update.value.account.lamports,
-                LAMPORTS_PER_SOL + TRANSFER_AMOUNT,
+                update.value.account.lamports, expected_lamports2,
                 "account 2 should have its balance increased"
             );
         }
