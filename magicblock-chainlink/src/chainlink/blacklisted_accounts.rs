602ec1625ddbe65262f03788d32ecdc9c47ea3ca
Thorsten Lorenz <thlorenz@gmx.de>
2025-10-20 13:51:04 +0200

    chore: include task context in blacklisted accounts

--- magicblock-chainlink/src/chainlink/blacklisted_accounts.rs
+++ magicblock-chainlink/src/chainlink/blacklisted_accounts.rs
@@ -24,8 +24,7 @@ pub fn blacklisted_accounts(
 
     blacklisted_accounts.insert(magic_program::ID);
     blacklisted_accounts.insert(magic_program::MAGIC_CONTEXT_PUBKEY);
-    // TODO(thlorenz: once we merge task PR add this
-    // blacklisted_accounts.insert(magic_program::TASK_CONTEXT_PUBKEY);
+    blacklisted_accounts.insert(magic_program::TASK_CONTEXT_PUBKEY);
     blacklisted_accounts.insert(*validator_id);
     blacklisted_accounts.insert(*faucet_id);
     blacklisted_accounts
