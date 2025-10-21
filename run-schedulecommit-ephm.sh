7b23df7447c806a05a92e08a6a5ff2eab77f15be
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2025-10-21 09:49:56 +0530

    Worked: cargo test --test 02_commit_and_undelegate test_committing_and_undelegating_one_account --profile test -- --nocapture --exact

---
+++ run-schedulecommit-ephm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash  
+
+RUST_LOG_STYLE="EPHEM" VALIDATOR_KEYPAIR="62LxqpAW6SWhp7iKBjCQneapn1w6btAhW7xHeREWSpPzw3xZbHCfAFesSR4R76ejQXCLWrndn37cKCCLFvx6Swps" "cargo" "run" "--" "/Users/snawaz/projects/mb/magicblock-validator/test-integration/configs/schedulecommit-conf-fees.ephem.toml"
