ec70e89f665cc3569cafd5dbd8831b9c35354d14
Thorsten Lorenz <thlorenz@gmx.de>
2025-10-27 11:00:52 +0100

    chore: fmt

--- magicblock-chainlink/src/remote_account_provider/chain_laser_actor.rs
+++ magicblock-chainlink/src/remote_account_provider/chain_laser_actor.rs
@@ -192,7 +192,10 @@ impl ChainLaserActor {
     }
 
     /// Handles an update from one of the account data streams.
-    async fn handle_account_update(&mut self, (pubkey, result): LaserStreamUpdate) {
+    async fn handle_account_update(
+        &mut self,
+        (pubkey, result): LaserStreamUpdate,
+    ) {
         match result {
             Ok(SubscribeUpdate {
                 update_oneof: Some(UpdateOneof::Account(acc)),
