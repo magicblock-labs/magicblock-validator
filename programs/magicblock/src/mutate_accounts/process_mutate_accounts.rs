50ac295ac3f1724756328345ea05b2ad8ff0df68
Dodecahedr0x <hexadecifish@gmail.com>
2025-11-19 11:06:02 +0100

    fix: mutate account assertion

--- programs/magicblock/src/mutate_accounts/process_mutate_accounts.rs
+++ programs/magicblock/src/mutate_accounts/process_mutate_accounts.rs
@@ -357,7 +357,7 @@ mod tests {
                 owner,
                 executable: false,
                 data,
-                rent_epoch: u64::MAX,
+                rent_epoch: 0,
             } => {
                 assert_eq!(lamports, AUTHORITY_BALANCE - 100);
                 assert_eq!(owner, system_program::id());
@@ -375,7 +375,7 @@ mod tests {
                 owner: owner_key,
                 executable: true,
                 data,
-                rent_epoch: u64::MAX,
+                rent_epoch: 88,
             } => {
                 assert_eq!(data, modification.data.unwrap());
                 assert_eq!(owner_key, modification.owner.unwrap());
