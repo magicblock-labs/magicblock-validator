47e9de898ebd56d36bf75eceaca9fc88b90bdc58
taco-paco <edwinswatpako@gmail.com>
2025-11-22 19:11:04 +0400

    fix: double undelegation

--- programs/magicblock/src/schedule_transactions/process_schedule_base_intent.rs
+++ programs/magicblock/src/schedule_transactions/process_schedule_base_intent.rs
@@ -14,7 +14,7 @@ use crate::{
     },
     schedule_transactions::check_magic_context_id,
     utils::{
-        account_actions::mark_account_as_undelegating,
+        account_actions::perform_account_undelegation,
         accounts::{
             get_instruction_account_with_idx, get_instruction_pubkey_with_idx,
         },
@@ -143,7 +143,7 @@ pub(crate) fn process_schedule_base_intent(
         // Once account is undelegated we need to make it immutable in our validator.
         for (pubkey, account_ref) in undelegated_accounts_ref.iter() {
             undelegated_pubkeys.push(pubkey.to_string());
-            mark_account_as_undelegating(account_ref);
+            perform_account_undelegation(account_ref);
         }
     }
     if !undelegated_pubkeys.is_empty() {
