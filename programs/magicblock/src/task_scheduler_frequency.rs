895b5493e351eccede8830b2848ac4c1198c74db
Dodecahedr0x <hexadecifish@gmail.com>
2025-12-03 13:42:23 +0100

    feat: use atomic i64

--- programs/magicblock/src/task_scheduler_frequency.rs
+++ programs/magicblock/src/task_scheduler_frequency.rs
@@ -1,22 +1,17 @@
-use std::sync::RwLock;
+use std::sync::atomic::{AtomicI64, Ordering};
 
 use lazy_static::lazy_static;
 use magicblock_config::consts;
 
 lazy_static! {
-    static ref MIN_TASK_SCHEDULER_INTERVAL: RwLock<i64> =
-        RwLock::new(consts::DEFAULT_TASK_SCHEDULER_MIN_INTERVAL_MILLIS);
+    static ref MIN_TASK_SCHEDULER_INTERVAL: AtomicI64 =
+        AtomicI64::new(consts::DEFAULT_TASK_SCHEDULER_MIN_INTERVAL_MILLIS);
 }
 
 pub fn min_task_scheduler_interval() -> i64 {
-    *MIN_TASK_SCHEDULER_INTERVAL
-        .read()
-        .expect("RwLock MIN_TASK_SCHEDULER_INTERVAL poisoned")
+    MIN_TASK_SCHEDULER_INTERVAL.load(Ordering::Relaxed)
 }
 
 pub fn set_min_task_scheduler_interval(interval: i64) {
-    let mut min_task_scheduler_interval_lock = MIN_TASK_SCHEDULER_INTERVAL
-        .write()
-        .expect("RwLock MIN_TASK_SCHEDULER_INTERVAL poisoned");
-    *min_task_scheduler_interval_lock = interval.max(1);
+    MIN_TASK_SCHEDULER_INTERVAL.store(interval.max(1), Ordering::Relaxed);
 }
