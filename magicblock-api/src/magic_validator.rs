be4644201eedbc2885309e7437b5ce5a09b0bb71
edwin <102215563+taco-paco@users.noreply.github.com>
2025-12-19 14:39:05 +0700

    fix: flush on preparation for shutdown (#753)

--- magicblock-api/src/magic_validator.rs
+++ magicblock-api/src/magic_validator.rs
@@ -700,6 +700,9 @@ impl MagicValidator {
         self.ledger_truncator.stop();
         // Calls & awaits until manual compaction is canceled
         self.ledger.cancel_manual_compactions();
+        if let Err(err) = self.ledger.flush() {
+            error!("Failed to flush during shutdown preparation: {:?}", err);
+        }
     }
 }
 
