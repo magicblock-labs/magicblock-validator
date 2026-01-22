0294544053795e1554a2783631d74592554201e6
Luca Cillario <luca.cillario.95@gmail.com>
2026-01-22 14:50:29 +0100

    debug

--- test-integration/test-runner/bin/run_tests.rs
+++ test-integration/test-runner/bin/run_tests.rs
@@ -790,7 +790,10 @@ fn run_test(
     if let Some(test) = config.test {
         cmd.arg(format!("'{}'", test));
     }
-    cmd.arg("--").arg("--test-threads=1").arg("--nocapture");
+    let test_threads = std::env::var("CARGO_TEST_THREADS").unwrap_or_else(|_| "1".to_string());
+    cmd.arg("--")
+        .arg(format!("--test-threads={}", test_threads))
+        .arg("--nocapture");
     cmd.current_dir(manifest_dir.clone());
     Teepee::new(cmd).output()
 }
