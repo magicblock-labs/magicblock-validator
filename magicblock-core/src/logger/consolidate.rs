c382b28b4a58a22ffe0f12d25a7a11f6a108cc18
Thorsten Lorenz <thlorenz@gmx.de>
2026-01-26 17:39:31 -0700

    chore: doc for latest consolidator

--- magicblock-core/src/logger/consolidate.rs
+++ magicblock-core/src/logger/consolidate.rs
@@ -34,6 +34,16 @@ pub fn log_trace_warn<T: Display, E: Debug>(
     }
 }
 
+/// Logs a trace message with data and error until the max_trace count is reached,
+/// at which point it logs a debug message with the error and count.
+/// Subsequent calls reset the trace count.
+/// # Arguments
+/// * `trace_msg` - The message to log at trace level.
+/// * `debug_msg` - The message to log at debug level.
+/// * `data` - The data to include in the trace log.
+/// * `err` - The error to include in both trace and debug logs.
+/// * `max_trace` - The maximum number of trace logs before logging a debug message.
+/// * `trace_count` - An atomic counter tracking the number of trace logs.
 pub fn log_trace_debug<T: Display, E: Debug>(
     trace_msg: &str,
     debug_msg: &str,
