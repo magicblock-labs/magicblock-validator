763d09a258bc21c8f1cbe3c990984f0e219ae509
Gabriele Picco <piccogabriele@gmail.com>
2026-01-28 14:56:36 +0100

    feat: startup time instrumentation and improvements

--- magicblock-api/src/magic_validator.rs
+++ magicblock-api/src/magic_validator.rs
@@ -5,6 +5,7 @@ use std::{
         Arc,
     },
     thread,
+    time::Instant,
 };
 
 use magicblock_account_cloner::{
@@ -144,21 +145,27 @@ impl MagicValidator {
             config.validator.basefee,
         );
 
+        let step_start = Instant::now();
         let (ledger, last_slot) =
             Self::init_ledger(&config.ledger, &config.storage)?;
+        log_timing("startup", "init_ledger", step_start);
         tracing::Span::current().record("last_slot", last_slot);
         info!("Ledger initialized");
         let ledger_path = ledger.ledger_path();
 
+        let step_start = Instant::now();
         Self::sync_validator_keypair_with_ledger(
             ledger_path,
             &identity_keypair,
             config.ledger.verify_keypair,
         )?;
+        log_timing("startup", "sync_validator_keypair", step_start);
 
         let latest_block = ledger.latest_block().load();
+        let step_start = Instant::now();
         let accountsdb =
             AccountsDb::new(&config.accountsdb, &config.storage, last_slot)?;
+        log_timing("startup", "accountsdb_init", step_start);
         for (pubkey, account) in genesis_config.accounts {
             let _ = accountsdb.insert_account(&pubkey, &account.into());
         }
@@ -178,22 +185,29 @@ impl MagicValidator {
 
         let accountsdb = Arc::new(accountsdb);
 
+        let step_start = Instant::now();
         let metrics_service = magicblock_metrics::try_start_metrics_service(
             config.metrics.address.0,
             token.clone(),
         )
         .map_err(ApiError::FailedToStartMetricsService)?;
+        log_timing("startup", "metrics_service_start", step_start);
 
+        let step_start = Instant::now();
         let system_metrics_ticker = init_system_metrics_ticker(
             config.metrics.collect_frequency,
             &ledger,
             &accountsdb,
             token.clone(),
         );
+        log_timing("startup", "system_metrics_ticker_start", step_start);
 
         let (mut dispatch, validator_channels) = link();
 
+        let step_start = Instant::now();
         let committor_service = Self::init_committor_service(&config).await?;
+        log_timing("startup", "committor_service_init", step_start);
+        let step_start = Instant::now();
         let chainlink = Arc::new(
             Self::init_chainlink(
                 &config,
@@ -205,6 +219,7 @@ impl MagicValidator {
             )
             .await?,
         );
+        log_timing("startup", "chainlink_init", step_start);
 
         let scheduled_commits_processor =
             committor_service.as_ref().map(|committor_service| {
@@ -216,6 +231,7 @@ impl MagicValidator {
                 ))
             });
 
+        let step_start = Instant::now();
         load_upgradeable_programs(
             &accountsdb,
             &programs_to_load(&config.programs),
@@ -223,6 +239,7 @@ impl MagicValidator {
         .map_err(|err| {
             ApiError::FailedToLoadProgramsIntoBank(format!("{:?}", err))
         })?;
+        log_timing("startup", "load_programs", step_start);
 
         validator::init_validator_authority(identity_keypair);
         let base_fee = config.validator.basefee;
@@ -257,15 +274,19 @@ impl MagicValidator {
         // runtime, -1 is taken up by the transaction scheduler itself
         let transaction_executors =
             (num_cpus::get() / 2).saturating_sub(1).max(1) as u32;
+        let step_start = Instant::now();
         let transaction_scheduler = TransactionScheduler::new(
             transaction_executors,
             txn_scheduler_state,
         );
+        log_timing("startup", "transaction_scheduler_init", step_start);
         info!(
             executor_count = transaction_executors,
             "Running execution backend"
         );
+        let step_start = Instant::now();
         let transaction_execution = transaction_scheduler.spawn();
+        log_timing("startup", "transaction_execution_spawn", step_start);
 
         let shared_state = SharedState::new(
             node_context,
@@ -273,6 +294,7 @@ impl MagicValidator {
             ledger.clone(),
             chainlink.clone(),
         );
+        let step_start = Instant::now();
         let rpc = initialize_aperture(
             &config.aperture,
             shared_state,
@@ -280,7 +302,9 @@ impl MagicValidator {
             token.clone(),
         )
         .await?;
+        log_timing("startup", "aperture_init", step_start);
         let rpc_handle = thread::spawn(move || {
+            let step_start = Instant::now();
             let workers = (num_cpus::get() / 2 - 1).max(1);
             let runtime = Builder::new_multi_thread()
                 .worker_threads(workers)
@@ -288,6 +312,7 @@ impl MagicValidator {
                 .thread_name("rpc-worker")
                 .build()
                 .expect("failed to bulid async runtime for rpc service");
+            log_timing("startup", "rpc_runtime_build", step_start);
             runtime.block_on(rpc.run());
 
             drop(runtime);
@@ -299,6 +324,7 @@ impl MagicValidator {
                 "ledger_path didn't have a parent, should never happen",
             ));
         debug!(path = %task_scheduler_db_path.display(), "Initializing task scheduler");
+        let step_start = Instant::now();
         let task_scheduler = TaskSchedulerService::new(
             &task_scheduler_db_path,
             &config.task_scheduler,
@@ -310,6 +336,7 @@ impl MagicValidator {
             ledger.latest_block().clone(),
             token.clone(),
         )?;
+        log_timing("startup", "task_scheduler_init", step_start);
 
         Ok(Self {
             accountsdb,
@@ -496,6 +523,7 @@ impl MagicValidator {
         // we have this number for our max blockhash age in slots, which correspond to 60 seconds
         let max_block_age =
             SOLANA_VALID_BLOCKHASH_AGE / self.config.ledger.block_time_ms();
+        let step_start = Instant::now();
         let slot_to_continue_at = process_ledger(
             &self.ledger,
             accountsdb_slot,
@@ -503,6 +531,7 @@ impl MagicValidator {
             max_block_age,
         )
         .await?;
+        log_timing("startup", "ledger_replay", step_start);
         self.accountsdb.set_slot(slot_to_continue_at);
 
         // The transactions to schedule and accept account commits re-run when we
@@ -521,6 +550,7 @@ impl MagicValidator {
         // We want the next transaction either due to hydrating of cloned accounts or
         // user request to be processed in the next slot such that it doesn't become
         // part of the last block found in the existing ledger which would be incorrect.
+        let step_start = Instant::now();
         let (update_ledger_result, _) = advance_slot_and_update_ledger(
             &self.accountsdb,
             &self.ledger,
@@ -529,6 +559,7 @@ impl MagicValidator {
         if let Err(err) = update_ledger_result {
             return Err(err.into());
         }
+        log_timing("startup", "advance_slot_after_replay", step_start);
 
         tracing::Span::current().record("ledger_slot", slot_to_continue_at);
         info!("Ledger processing complete");
@@ -577,25 +608,28 @@ impl MagicValidator {
         .inspect(|_| info!("Unregistered validator on chain"))
     }
 
-    async fn ensure_validator_funded_on_chain(&self) -> ApiResult<()> {
+    async fn ensure_validator_funded_on_chain(
+        rpc_url: String,
+        identity: Pubkey,
+    ) -> ApiResult<()> {
         // NOTE: 5 SOL seems reasonable, but we may require a different amount in the future
         const MIN_BALANCE_SOL: u64 = 5;
 
         let lamports = RpcClient::new_with_commitment(
-            self.config.rpc_url().to_owned(),
+            rpc_url,
             CommitmentConfig::confirmed(),
         )
-        .get_balance(&self.identity)
+        .get_balance(&identity)
         .await
         .map_err(|err| {
             ApiError::FailedToObtainValidatorOnChainBalance(
-                self.identity,
+                identity,
                 err.to_string(),
             )
         })?;
         if lamports < MIN_BALANCE_SOL * LAMPORTS_PER_SOL {
             Err(ApiError::ValidatorInsufficientlyFunded(
-                self.identity,
+                identity,
                 MIN_BALANCE_SOL,
             ))
         } else {
@@ -606,19 +640,47 @@ impl MagicValidator {
     #[instrument(skip(self))]
     pub async fn start(&mut self) -> ApiResult<()> {
         if matches!(self.config.lifecycle, LifecycleMode::Ephemeral) {
-            self.ensure_validator_funded_on_chain().await?;
+            let rpc_url = self.config.rpc_url().to_owned();
+            let identity = self.identity;
+            tokio::spawn(async move {
+                let step_start = Instant::now();
+                let result = MagicValidator::ensure_validator_funded_on_chain(
+                    rpc_url, identity,
+                )
+                .await;
+                log_timing(
+                    "startup_background",
+                    "ensure_funded_on_chain",
+                    step_start,
+                );
+                if let Err(err) = result {
+                    error!(error = ?err, "Validator balance check failed");
+                    error!("Exiting process");
+                    std::process::exit(1);
+                }
+            });
             if let Some(ref config) = self.config.chain_operation {
+                let step_start = Instant::now();
                 self.register_validator_on_chain(config).await?;
+                log_timing(
+                    "startup",
+                    "register_validator_on_chain",
+                    step_start,
+                );
             }
         }
 
         // Ledger processing needs to happen before anything of the below
+        let step_start = Instant::now();
         self.maybe_process_ledger().await?;
+        log_timing("startup", "maybe_process_ledger", step_start);
 
         // Ledger replay has completed, we can now clean non-delegated accounts
         // including programs from the bank
         if !self.config.accountsdb.reset {
+            let step_start = Instant::now();
             self.chainlink.reset_accounts_bank()?;
+            log_timing("startup", "reset_accounts_bank", step_start);
         }
 
         // Now we are ready to start all services and are ready to accept transactions
@@ -629,10 +691,13 @@ impl MagicValidator {
             .filter(|co| !co.claim_fees_frequency.is_zero())
             .map(|co| co.claim_fees_frequency)
         {
+            let step_start = Instant::now();
             self.claim_fees_task
                 .start(frequency, self.config.rpc_url().to_owned());
+            log_timing("startup", "claim_fees_task_start", step_start);
         }
 
+        let step_start = Instant::now();
         self.slot_ticker = Some(init_slot_ticker(
             self.accountsdb.clone(),
             &self.scheduled_commits_processor,
@@ -642,8 +707,11 @@ impl MagicValidator {
             self.block_udpate_tx.clone(),
             self.exit.clone(),
         ));
+        log_timing("startup", "slot_ticker_start", step_start);
 
+        let step_start = Instant::now();
         self.ledger_truncator.start();
+        log_timing("startup", "ledger_truncator_start", step_start);
 
         // TODO: we should shutdown gracefully.
         // This is discussed in this comment:
@@ -656,6 +724,7 @@ impl MagicValidator {
             .take()
             .expect("task_scheduler should be initialized");
         tokio::spawn(async move {
+            let step_start = Instant::now();
             let join_handle = match task_scheduler.start().await {
                 Ok(join_handle) => join_handle,
                 Err(err) => {
@@ -664,6 +733,7 @@ impl MagicValidator {
                     std::process::exit(1);
                 }
             };
+            log_timing("startup", "task_scheduler_start", step_start);
             match join_handle.await {
                 Ok(Ok(())) => {}
                 Ok(Err(err)) => {
@@ -685,6 +755,7 @@ impl MagicValidator {
 
     #[instrument(skip(self))]
     pub async fn stop(mut self) {
+        let stop_start = Instant::now();
         self.exit.store(true, Ordering::Relaxed);
 
         // Ordering is important here
@@ -693,37 +764,62 @@ impl MagicValidator {
         if let Some(ref scheduled_commits_processor) =
             self.scheduled_commits_processor
         {
+            let step_start = Instant::now();
             scheduled_commits_processor.stop();
+            log_timing(
+                "shutdown",
+                "scheduled_commits_processor_stop",
+                step_start,
+            );
         }
         if let Some(ref committor_service) = self.committor_service {
+            let step_start = Instant::now();
             committor_service.stop();
+            log_timing("shutdown", "committor_service_stop", step_start);
         }
 
+        let step_start = Instant::now();
         self.claim_fees_task.stop().await;
+        log_timing("shutdown", "claim_fees_task_stop", step_start);
 
         if self.config.chain_operation.is_some()
             && matches!(self.config.lifecycle, LifecycleMode::Ephemeral)
         {
+            let step_start = Instant::now();
             if let Err(err) = self.unregister_validator_on_chain() {
                 error!(error = ?err, "Failed to unregister");
             }
+            log_timing("shutdown", "unregister_validator_on_chain", step_start);
         }
         // we have two memory mapped databases,
         // flush them to disk before exitting
+        let step_start = Instant::now();
         self.accountsdb.flush();
+        log_timing("shutdown", "accountsdb_flush", step_start);
 
+        let step_start = Instant::now();
         if let Err(err) = self.ledger.shutdown(true) {
             error!(error = ?err, "Failed to shutdown ledger");
         }
+        log_timing("shutdown", "ledger_shutdown", step_start);
+        let step_start = Instant::now();
         let _ = self.rpc_handle.join();
+        log_timing("shutdown", "rpc_thread_join", step_start);
         if let Some(handle) = self.slot_ticker {
+            let step_start = Instant::now();
             let _ = handle.await;
+            log_timing("shutdown", "slot_ticker_join", step_start);
         }
+        let step_start = Instant::now();
         if let Err(err) = self.ledger_truncator.join() {
             error!(error = ?err, "Ledger truncator did not gracefully exit");
         }
+        log_timing("shutdown", "ledger_truncator_join", step_start);
+        let step_start = Instant::now();
         let _ = self.transaction_execution.join();
+        log_timing("shutdown", "transaction_execution_join", step_start);
 
+        log_timing("shutdown", "stop_total", stop_start);
         info!("MagicValidator shutdown");
     }
 
@@ -735,13 +831,20 @@ impl MagicValidator {
     /// This speeds up `stop` as it doesn't have to await for compaction cancellation
     /// Calling this still allows to write or read from DB
     pub fn prepare_ledger_for_shutdown(&mut self) {
+        let step_start = Instant::now();
         self.ledger_truncator.stop();
         // Calls & awaits until manual compaction is canceled
         self.ledger.cancel_manual_compactions();
         if let Err(err) = self.ledger.flush() {
             error!(error = ?err, "Failed to flush during shutdown preparation");
         }
+        log_timing("shutdown", "prepare_ledger_for_shutdown", step_start);
     }
+}
+
+fn log_timing(phase: &'static str, step: &'static str, start: Instant) {
+    let duration_ms = start.elapsed().as_millis() as u64;
+    debug!(phase, step, duration_ms, "Validator timing");
 }
 
 fn programs_to_load(programs: &[LoadableProgram]) -> Vec<(Pubkey, PathBuf)> {
