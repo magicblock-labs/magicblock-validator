4140eb5c0b245b0e32d692a7c9a3f66ecb5eb5dc
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2026-03-11 15:37:36 +0530

    re-implement CommitFinalize task conforming to the new task design

--- magicblock-committor-service/src/tasks/mod.rs
+++ magicblock-committor-service/src/tasks/mod.rs
@@ -17,13 +17,12 @@ use magicblock_committor_program::{
     },
     pdas, ChangesetChunks, Chunks,
 };
-use magicblock_core::intent::CommittedAccount;
 use magicblock_metrics::metrics::LabelValue;
 use magicblock_program::magic_scheduled_base_intent::BaseAction;
-use solana_account::Account;
 use solana_instruction::{AccountMeta, Instruction};
 use solana_pubkey::Pubkey;
 
+pub mod commit_finalize_task;
 pub mod commit_task;
 pub mod task_builder;
 pub mod task_strategist;
@@ -31,6 +30,7 @@ pub mod utils;
 
 pub use task_builder::TaskBuilderImpl;
 
+use crate::tasks::commit_finalize_task::CommitFinalizeTask;
 use crate::tasks::commit_task::CommitTask;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -51,6 +51,7 @@ pub enum TaskStrategy {
 #[derive(Clone, Debug)]
 pub enum BaseTaskImpl {
     Commit(CommitTask),
+    CommitFinalize(CommitFinalizeTask),
     Finalize(FinalizeTask),
     Undelegate(UndelegateTask),
     BaseAction(BaseActionTask),
@@ -64,6 +65,7 @@ impl BaseTask for BaseTaskImpl {
     fn instruction(&self, validator: &Pubkey) -> Instruction {
         match self {
             Self::Commit(value) => value.instruction(validator),
+            Self::CommitFinalize(value) => value.instruction(validator),
             Self::Finalize(value) => value.instruction(validator),
             Self::Undelegate(value) => value.instruction(validator),
             Self::BaseAction(value) => value.instruction(validator),
@@ -80,6 +82,7 @@ impl BaseTask for BaseTaskImpl {
     fn compute_units(&self) -> u32 {
         match self {
             Self::Commit(value) => value.compute_units(),
+            Self::CommitFinalize(value) => value.compute_units(),
             Self::BaseAction(value) => value.compute_units(),
             Self::Finalize(_) => 70_000,
             Self::Undelegate(_) => 70_000,
@@ -89,6 +92,7 @@ impl BaseTask for BaseTaskImpl {
     fn accounts_size_budget(&self) -> u32 {
         match self {
             Self::Commit(value) => value.accounts_size_budget(),
+            Self::CommitFinalize(value) => value.accounts_size_budget(),
             Self::BaseAction(value) => value.accounts_size_budget(),
             Self::Finalize(_) => finalize_size_budget(AccountSizeClass::Huge),
             Self::Undelegate(_) => {
@@ -115,6 +119,13 @@ impl LabelValue for BaseTaskImpl {
                     "buffer_commit"
                 } else {
                     "args_commit"
+                }
+            }
+            Self::CommitFinalize(task) => {
+                if task.is_buffer() {
+                    "buffer_commit_finalize"
+                } else {
+                    "args_commit_finalize"
                 }
             }
             Self::Finalize(_) => "args_finalize",
@@ -154,22 +165,6 @@ pub trait BaseTask: Send + Sync + Clone {
 
     /// Returns the max accounts-data-size that can be used with SetLoadedAccountsDataSizeLimit
     fn accounts_size_budget(&self) -> u32;
-}
-
-#[derive(Clone, Debug)]
-pub struct CommitDiffTask {
-    pub commit_id: u64,
-    pub allow_undelegation: bool,
-    pub committed_account: CommittedAccount,
-    pub base_account: Account,
-}
-
-#[derive(Clone, Debug)]
-pub struct CommitFinalizeTask {
-    pub commit_id: u64,
-    pub allow_undelegation: bool,
-    pub base_account: Option<Account>, // None implies commit-full-bytes else commit-diff
-    pub committed_account: CommittedAccount,
 }
 
 #[derive(Clone, Debug)]
@@ -276,6 +271,8 @@ impl BaseActionTaskV1 {
                 is_signer: false,
             })
             .collect();
+
+        #[allow(deprecated)]
         dlp::instruction_builder::call_handler(
             *validator,
             action.destination_program,
