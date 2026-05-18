807af8261abbfeb584c56da63d4329c72a36969e
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2026-05-19 00:01:49 +0530

    tested with examples

--- magicblock-magic-program-api/src/compat.rs
+++ magicblock-magic-program-api/src/compat.rs
@@ -1,6 +1,3 @@
-//#[cfg(not(any(feature = "latest", feature = "backward-compat")))]
-//compile_error!("enable either the `latest` or `backward-compat` feature");
-
 #[cfg(feature = "backward-compat")]
 mod selected {
     pub use solana_program_compat::{
