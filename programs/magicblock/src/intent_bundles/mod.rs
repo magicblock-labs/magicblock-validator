a0e4da1203127f88195ccd835e51b1c7d6523a8e
taco-paco <edwinswatpako@gmail.com>
2026-06-03 17:11:45 +0700

    wip

--- programs/magicblock/src/intent_bundles/mod.rs
+++ programs/magicblock/src/intent_bundles/mod.rs
@@ -1,5 +1,7 @@
 pub mod magic_scheduled_base_intent;
 pub mod outbox;
+// TODO(edwin): rename? 
 pub mod outbox_intent_bundles;
+// TODO(edwin): rename?
 mod process_execute_callback;
 pub mod schedule;
